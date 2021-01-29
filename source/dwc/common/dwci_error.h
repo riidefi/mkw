1f9433a1c2eaad045c922453c2665f98d68b82fc
riidefi <34194588+riidefi@users.noreply.github.com>
2021-01-28 22:51:38 -0500

    :sparkles: DWC: Decompiled dwc_error.c

---
+++ source/dwc/common/dwci_error.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	
+enum dwcError { DWCErrorNone = 0, DWCErrorFatal = 9 };
+
+
+//! @brief @return Return if there is an error.
+//!
+int DWCi_IsError();
+
+//! @brief Set the static error data.
+//!
+//! @details   Cannot proceed if the last error before this function is called
+//! is fatal.
+//!
+//! @param[in] lastError The last error encountered.
+//! @param[in] errorCode Error code.
+//!
+void DWCi_SetError(int lastError, int errorCode);
+
+#ifdef __cplusplus
+}
+#endif
