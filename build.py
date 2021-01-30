0972fe22deb2f807be62a01951f319fa9fa159bb
riidefi <34194588+riidefi@users.noreply.github.com>
2021-01-30 12:47:42 -0500

    :sparkles: Decompiled eggArchive.cpp

--- build.py
+++ build.py
@@ -118,6 +118,7 @@ def build():
 
 	compile_source("source/dwc/common/dwc_error.c", "out/dwc_error.o", '4199_60831', RVL_OPTS)
 
+	compile_source("source/egg/core/eggArchive.cpp", "out/eggArchive.o", '4201_127', EGG_OPTS + " -use_lmw_stmw=on ")
 	compile_source("source/egg/core/eggDisposer.cpp", "out/eggDisposer.o", '4201_127', EGG_OPTS)
 	compile_source("source/egg/core/eggHeap.cpp", "out/eggHeap.o", '4201_127', EGG_OPTS + " -ipa file -use_lmw_stmw=on  ")
 	compile_source("source/egg/core/eggStreamDecomp.cpp", "out/eggStreamDecomp.o", '4201_127', EGG_OPTS)
