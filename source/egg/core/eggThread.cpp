f3458f7cbc8470be858a479e95883587f58bed22
riidefi <34194588+riidefi@users.noreply.github.com>
2021-02-01 08:46:34 -0500

    :sparkles: Decompiled eggThread.cpp

---
+++ source/egg/core/eggThread.cpp
@@ -0,0 +1,111 @@
+#include <egg/core/eggHeap.hpp>
+#include <egg/core/eggThread.hpp>
+#include <egg/eggInternal.hpp>
+
+namespace EGG {
+
+nw4r::ut::List Thread::sThreadList;
+
+Thread::Thread(u32 stackSize, int msgCount, int prio, Heap* heap) {
+  if (!heap)
+    heap = Heap::sCurrentHeap;
+
+  mContainingHeap = heap;
+  mStackSize = ROUND_DOWN(stackSize, 32);
+  mStackMemory =
+      reinterpret_cast<char*>(Heap::alloc(mStackSize, 32, mContainingHeap));
+  EGG_ASSERT(mStackMemory, "eggThread.cpp", 70, "mStackMemory");
+
+  mOSThread = Heap::alloc<OSThread>(mContainingHeap, 32);
+  EGG_ASSERT(mOSThread, "eggThread.cpp", 80, "mOSThread");
+
+  OSCreateThread(mOSThread, start,
+                 this,                      // argument for start
+                 mStackMemory + mStackSize, // the created stackBase is the
+                                            // very highest value
+                 mStackSize, prio, 1);
+
+  mAlloctableHeap = nullptr;
+  setCommonMesgQueue(msgCount, mContainingHeap);
+}
+
+Thread::Thread(OSThread* osThread, int msgCount)
+    : mContainingHeap(nullptr), mOSThread(osThread) {
+  mStackSize = (char*)osThread->stack_low - (char*)osThread->stack_high;
+  mStackMemory = osThread->stack_high;
+
+  mAlloctableHeap = nullptr;
+  setCommonMesgQueue(msgCount, Heap::sCurrentHeap);
+}
+
+Thread::~Thread() {
+  nw4r::ut::List_Remove(&sThreadList, this);
+
+  if (mContainingHeap != nullptr) {
+    if (!OSIsThreadTerminated(mOSThread)) {
+      OSDetachThread(mOSThread);
+      OSCancelThread(mOSThread);
+    }
+
+    Heap::free(mStackMemory, mContainingHeap);
+    Heap::free(mOSThread, mContainingHeap);
+  }
+
+  Heap::free(mMesgBuffer, nullptr);
+}
+
+Thread* Thread::findThread(OSThread* osThread) {
+  Thread* iterThread = nullptr;
+
+  while ((iterThread = (Thread*)List_GetNext(&sThreadList, iterThread)))
+    if (iterThread->mOSThread == osThread)
+      return iterThread;
+
+  return nullptr;
+}
+
+void Thread::kandoTestCancelAllThread() {
+  Thread* iterThread = nullptr; // r30
+
+  while ((iterThread = (Thread*)List_GetNext(&sThreadList, iterThread))) {
+    OSThread* cur = OSGetCurrentThread();
+    OSThread* thread = iterThread->mOSThread;
+    if (cur != thread)
+      OSCancelThread(thread);
+  }
+}
+
+void Thread::initialize() {
+  // offsetof(Thread, nw4r::ut::Node)
+  List_Init(&sThreadList, 0x40);
+  OSSetSwitchThreadCallback(switchThreadCallback);
+}
+
+void Thread::switchThreadCallback(OSThread* from, OSThread* to) {
+  // findThread inlined twice
+  Thread* fromThread = from ? findThread(from) : nullptr; // r30
+  Thread* toThread = to ? findThread(to) : nullptr;       // r31
+
+  if (fromThread != nullptr)
+    fromThread->onExit();
+
+  if (toThread != nullptr)
+    toThread->onEnter();
+}
+
+void Thread::setCommonMesgQueue(int msgCount, EGG::Heap* heap) {
+  mMesgCount = msgCount;
+  mMesgBuffer = Heap::alloc<OSMessage>(msgCount, heap);
+  EGG_ASSERT(mMesgBuffer, "eggThread.cpp", 262, "mMesgBuffer");
+
+  OSInitMessageQueue(&mMesgQueue, mMesgBuffer, mMesgCount);
+  List_Append(&sThreadList, this);
+}
+
+void* Thread::start(void* eggThread) {
+  return reinterpret_cast<Thread*>(eggThread)->run();
+}
+
+void* Thread::run() { return nullptr; }
+
+} // namespace EGG
---
+++ source/egg/core/eggThread.hpp
@@ -0,0 +1,143 @@
+/**
+ * @file
+ * @brief Thread class for extending.
+ */
+
+#pragma once
+
+#include <nw4r/ut/utList.hpp>
+#include <rk_types.h>
+#include <rvl/os/osThread.h>
+
+namespace EGG {
+
+class Heap;
+
+//-------------------------------------------------------------	--------------
+//! @brief   Wrapper for OSThread.
+//---------------------------------------------------------------------------
+class Thread {
+public:
+  virtual ~Thread(); //!< [vt+0x08] Destroys the EGG Thread, freeing all used
+                     //!< memory and canceling the OSThread
+
+  virtual void* run();           //!< [vt+0x0C]
+  virtual void onEnter(); /*{}*/ //!< [vt+0x14]
+  virtual void onExit(); /*{}*/  //!< [vt+0x10]
+
+  Heap* mContainingHeap; //!< [+0x04] Heap to use for thread-specific
+                         //!< allocations (in ctor: create stack and OSThread)
+  OSThread* mOSThread;   //!< [+0x08] The OS thread this object wraps. Name
+                         //!< confirmed by WS assert.
+
+  OSMessageQueue mMesgQueue; //!< [+0x0C] sizeof=0x20
+  OSMessage* mMesgBuffer;    //!< [+0x2C] name confirmed WS assert
+  int mMesgCount;            //!< [+0x30]
+
+  char* mStackMemory; //!< [+0x34] The base (*beginning*) of the stack.
+  u32 mStackSize;     //!< [+0x38] The size of the stack.
+
+  Heap* mAlloctableHeap; //!< [0+x3C] When not NULL will override the heap used
+                         //!< for allocations. (Note: Does not escape the
+                         //!< Heap::sAllocatableHeap restriction.)
+  nw4r::ut::Node mLink;
+  //! @brief   	A constructor.
+  //!
+  //! @details	Creates an EGG::Thread and OSThread from arguments.
+  //!
+  //! @param[in]	stackSize:	The size of the stack to create.
+  //! @param[in] 	msgCount:	The maximum number of messages the
+  //! buffer can hold.
+  //! @param[in]	prio:		Priority of the OSThread to create. 0 is
+  //! highest priority; 31 is lowest priority.
+  //!							The default thread has a
+  //! priority of 16.
+  //!
+  //! @param[in]	heap:		Heap to use for allocations.
+  //!
+  Thread(u32 stackSize, int msgCount, int prio, Heap* heap);
+
+  //! @brief   	A constructor.
+  //!
+  //! @details	Creates an EGG::Thread to wrap the provided OSThread.
+  //!
+  //! @param[in]	osThread: 	OS thread to wrap.
+  //! @param[in] 	msgCount:	The maximum number of messages the
+  //! buffer can hold.
+  //!
+  Thread(OSThread* osThread, int msgCount);
+
+  //! @brief   	Find the (first) EGG::Thread that matches the provided osThread.
+  //!
+  //! @details	Iterate through the static thread list (sThreadList) to find the
+  //! first EGG thread
+  //!				that wraps the specified OS thread.
+  //!
+  //! @param[in]	osThread: 	The OS thread to search for.
+  //!
+  //! @return 	The found EGG thread.
+  //! @retval		NULL: No matching EGG thread was found.
+  //!
+  static Thread* findThread(OSThread* osThread);
+
+  //! @brief   	Cancel all but the current thread.
+  //!
+  //! @details	Iterates through registed threads in sThreadList, checking
+  //! against
+  //!				OSGetCurrentThread(), then calling
+  //! OSCancelThread().
+  //!
+  static void kandoTestCancelAllThread();
+
+  //! @brief   	Initialize EGG Thread.
+  //!
+  //! @details	Initializes sThreadList and sets the SwitchThreadCallback to
+  //!				Thread::switchThreadCallback.
+  //!
+  static void initialize();
+
+  //! @brief   	The callback for switching threads.
+  //!
+  //! @details	Finds the associated EGG threads of the two OSThreads, and calls
+  //! onExit()
+  //!				on the first thread and onEnter() on the next.
+  //!
+  //! @param[in]	from: 	The last thread running.
+  //! @param[in] 	to:		The next thread to run.
+  //!
+  //! @note		Either threads may be null if there is no thread to run.
+  //!				This function handles this accordingly
+  //!
+  static void switchThreadCallback(OSThread* from, OSThread* to);
+
+  //! @brief   	Configures the message queue.
+  //!
+  //! @details	- Allocates mMesgBuffer to msgCount * 4
+  //!				- Calls OSInitMessageQueue using mMesgQueue and
+  //! mMesgBuffer
+  //!				- Appends this Thread to sThreadList
+  //!
+  //! @param[in]	msgCount: 	The maximum number of messages the
+  //! buffer can hold.
+  //!							The buffer size in bytes
+  //! will be msgCount
+  //!* 4.
+  //!
+  void setCommonMesgQueue(int msgCount, Heap* heap);
+
+  //! @brief   	Starts a thread.
+  //!
+  //! @details	Casts eggThread to EGG::Thread* and calls the virtual run
+  //! method.
+  //!
+  //! @param[in]	eggThread: 	Pointer to an object that inherits
+  //! Thread.
+  //!
+  static void* start(void* eggThread);
+
+private:
+  // List of all registered threads.
+  static nw4r::ut::List sThreadList;
+};
+
+} // namespace EGG
