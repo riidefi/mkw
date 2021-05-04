da86e69ddcaef47c008e9242324d56f7c5dfab21
riidefi <34194588+riidefi@users.noreply.github.com>
2021-05-04 14:36:57 -0600

    :construction: Started JmpResourceCourse.cpp

---
+++ source/game/jmap/JmpResourceCourse.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <rk_types.h>
+
+namespace Field {
+
+class JmpResourceCourse {
+public:
+  static JmpResourceCourse* initStaticInstance();
+  static void destroyStaticInstance();
+  
+  static inline JmpResourceCourse* instance() {
+    return spInstance;
+  }
+
+private:
+  JmpResourceCourse();
+  virtual ~JmpResourceCourse();
+
+  static JmpResourceCourse* spInstance;
+
+  void* mpCourse;
+  
+  void* mpKartPoint;
+  void* mpEnemyPath;
+  void* mpEnemyPoint;
+  void* mpItemPath;
+  void* mpItemPoint;
+  void* mpCheckPath;
+  void* mpCheckPoint;
+  void* mpPointInfo;
+  void* mpGeoObj;
+  void* mpArea;
+  void* mpCamera;
+  void* mpJugemPoint;
+  void* mpCannonPoint;
+  void* mpStageInfo;
+  void* mpMissionPoint;
+
+  void* mGoalCamera;
+  void* mType9Camera;
+  void* mOpeningPanCamera;
+  unk _50;
+};
+
+} // namespace Field
