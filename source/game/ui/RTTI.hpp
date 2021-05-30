a6b6d19673367d3ba70f277e50915a2b48d5ca20
riidefi <34194588+riidefi@users.noreply.github.com>
2021-05-30 08:56:36 -0600

    :construction: UIControl.hpp

---
+++ source/game/ui/RTTI.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace UI {
+
+struct TypeInfo {
+  TypeInfo* base;
+};
+
+template <typename T> struct Deferred {
+  char data[sizeof(T)];
+
+  T& get() { return *reinterpret_cast<T*>(&data); }
+  const T& get() const { return *reinterpret_cast<const T*>(&data); }
+  
+  operator T&() { return get(); };
+  operator const T&() const { return get(); };
+};
+
+} // namespace UI
