11fa0dda3894efde43702c9c95c290d79da665f5
CIA gangstalker <iguana9999@protonmail.com>
2021-06-06 23:50:31 +0200

    Progress nw4r::ut List

--- source/nw4r/ut/utList.cpp
+++ source/nw4r/ut/utList.cpp
@@ -6,6 +6,80 @@ namespace ut {
 #define LIST_GET_ELEM(list, obj)                                               \
   ((nw4r::ut::Node*)(((u32)(obj)) + (list)->intrusion_offset))
 
+void List_Init(List* pList, u16 intrusion_offset) {
+  pList->head = nullptr;
+  pList->tail = nullptr;
+  pList->count = 0;
+  pList->intrusion_offset = intrusion_offset;
+}
+
+void List_Append(List* pList, void* pObj) {
+  if (pList->head == nullptr) {
+    // Node is the only element in the list.
+    // Therefore it is both head and tail.
+    nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
+    node->succ = nullptr;
+    node->pred = nullptr;
+    pList->head = pObj;
+    pList->tail = pObj;
+    pList->count++;
+  } else {
+    // Link node to end of list.
+    nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
+    node->pred = pList->tail;
+    node->succ = nullptr;
+    // Link second to last node to new tail.
+    LIST_GET_ELEM(pList, pList->tail)->succ = pObj;
+    // Set new tail.
+    pList->tail = pObj;
+    pList->count++;
+  }
+}
+
+static void List_Prepend(List* pList, void* pObj) {
+  if (pList->head == nullptr) {
+    // Node is the only element in the list.
+    // Therefore it is both head and tail.
+    nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
+    node->succ = nullptr;
+    node->pred = nullptr;
+    pList->head = pObj;
+    pList->tail = pObj;
+    pList->count++;
+  } else {
+    // Link node to start of list.
+    nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
+    node->pred = nullptr;
+    node->succ = pList->head;
+    // Link second node to new head.
+    LIST_GET_ELEM(pList, pList->head)->pred = pObj;
+    // Set new head.
+    pList->head = pObj;
+    pList->count++;
+  }
+}
+
+void List_Insert(List* pList, void* pTgt, void* pObj) {
+  if (pTgt == nullptr) {
+    // A null target means inserting at the end of the list.
+    List_Append(pList, pObj);
+  } else if (pTgt == pList->head) {
+    List_Prepend(pList, pObj);
+  } else {
+    nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
+    void* pred = LIST_GET_ELEM(pList, pTgt)->pred;
+    nw4r::ut::Node* predNode = LIST_GET_ELEM(pList, pred);
+    // Update links of node to be inserted.
+    node->pred = pred;
+    node->succ = pTgt;
+    // Update links of neighbors.
+    predNode->succ = pObj;
+    LIST_GET_ELEM(pList, pTgt)->pred = pObj;
+    // Update list.
+    pList->count++;
+  }
+}
+
 void List_Remove(List* pList, void* pObj) {
   nw4r::ut::Node* node = LIST_GET_ELEM(pList, pObj);
   if (node->pred == nullptr)
