337be72fd6d4f5df800f5337e2f672e34de7e291
CIA gangstalker <85429806+wait-wtf@users.noreply.github.com>
2021-06-19 19:31:03 +0200

    Progress nw4r::math (#15)

---
+++ asm/dol/sdata2_80387ea4.s
@@ -0,0 +1,5 @@
+
+.include "macros.inc"
+
+.section .sdata2, "a" # 0x80387EA4 - 0x80387EA8
+.4byte 0x00000000
---
+++ source/nw4r/math/mathTriangular.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <rk_types.h>
+
+namespace nw4r {
+namespace math {
+
+namespace detail {
+struct SinCosSample {
+  f32 sin_val;
+  f32 cos_val;
+  f32 sin_delta;
+  f32 cos_delta;
+};
+
+extern const SinCosSample gSinCosTbl[256 + 1];
+} // namespace detail
+
+// PAL 0x80085110
+f32 SinFIdx(f32 fidx);
+
+// PAL 0x80085180
+f32 CosFIdx(f32 fidx);
+
+// PAL 0x800851e0
+void SinCosFIdx(register f32*, register f32*, register f32);
+
+// PAL 0x80085250
+f32 AtanFIdx(f32);
+
+// PAL: 0x800853c0
+f32 Atan2FIdx(f32, f32);
+
+} // namespace math
+} // namespace nw4r
