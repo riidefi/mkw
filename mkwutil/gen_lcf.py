e7b21bc56645374d7fc52b678c1c58d9ac9874be
Richard Patel <me@terorie.dev>
2021-06-19 18:41:26 +0200

    Python Refactor (#13)
    * Project restructuring
    
    * Fix README.md
    
    * Fix README.md
    
    * Fix README.md
    
    * Add structure.md

---
+++ mkwutil/gen_lcf.py
@@ -0,0 +1,46 @@
+import argparse
+from pathlib import Path
+
+
+def gen_lcf(src, dst, object_paths):
+    lcf = ""
+
+    with open(src, "r") as f:
+        lcf = f.read()
+        lcf += "\nFORCEFILES {\n"
+        for obj_path in object_paths:
+            obj_path = Path(obj_path)
+            # TODO: Add ability to disable FORCEFILE to slices.csv
+            if obj_path.stem == "eggVideo":
+                continue
+            lcf += str(obj_path.parent / (obj_path.stem + ".o")) + "\n"
+        lcf += "}\n"
+
+    with open(dst, "w") as f:
+        f.write(lcf)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--base", type=Path, required=True, help="Path to base linker script"
+    )
+    parser.add_argument(
+        "--out", type=Path, required=True, help="Path to linker script output"
+    )
+    parser.add_argument("--objs", type=Path, required=True, help="Path to objects list")
+    parser.add_argument(
+        "--prefix",
+        type=Path,
+        default="",
+        help="Dir prefix for references in linker script",
+    )
+    args = parser.parse_args()
+
+    # Read list of objects.
+    objs = []
+    with open(args.objs, "r") as file:
+        for line in file:
+            objs.append(args.prefix / Path(line))
+
+    gen_lcf(args.base, args.out, objs)
---
+++ mkwutil/verify_main_dol.py
@@ -0,0 +1,108 @@
+"""
+Script to verify the target main.dol for authenticity.
+"""
+
+import argparse
+import hashlib
+from pathlib import Path
+
+from .dol import DolBinary
+
+
+def format_segment(name, at, at2, want_size, have_size, tag):
+    return "%10s: at_src=0x%08x at_dst=0x%08x want=0x%08x have=0x%08x [%s]" % (
+        name,
+        at,
+        at2,
+        want_size,
+        have_size,
+        tag,
+    )
+
+
+def verify_dol(reference, target):
+    """Verifies the target main.dol for authenticity."""
+    print("[DOL] Verifying...")
+    content = open(target, "rb").read()
+    ctx = hashlib.sha1(content)
+    digest = ctx.hexdigest()
+    if digest.lower() == "ac7d72448630ade7655fc8bc5fd7a6543cb53a49":
+        print("[DOL] Everything went okay! Output is matching! ^^")
+        return
+
+    want_len = 2766496
+    if len(content) != want_len:
+        print(
+            "Mismatched file size: Got %d (%+d)"
+            % (len(content), len(content) - want_len)
+        )
+
+    good = DolBinary(reference)
+    bad = DolBinary(target)
+
+    text_names = ["init", "text"]
+    data_names = [
+        "extab",
+        "extabindex",
+        "ctors",
+        "dtors",
+        "rodata",
+        "data",
+        "sdata",
+        "sdata2",
+    ]
+
+    for i, sizes in enumerate(zip(good.text_size, bad.text_size)):
+        if sizes[0] == 0 and sizes[1] == 0:
+            continue
+        good_segment = good.get_text_segment(i)
+        bad_segment = bad.get_text_segment(i)
+        match = good_segment == bad_segment
+        tag = "OK" if match else "FAIL"
+        if len(good_segment) != len(bad_segment):
+            tag = "SIZE"
+        print(
+            format_segment(
+                text_names[i],
+                good.text_segs[i].begin,
+                bad.text_segs[i].begin,
+                sizes[0],
+                sizes[1],
+                tag,
+            )
+        )
+    for i, sizes in enumerate(zip(good.data_size, bad.data_size)):
+        if sizes[0] == 0 and sizes[1] == 0:
+            continue
+        good_segment = good.get_data_segment(i)
+        bad_segment = bad.get_data_segment(i)
+        match = good_segment == bad_segment
+        tag = "OK" if match else "FAIL"
+        if len(good_segment) != len(bad_segment):
+            tag = "SIZE"
+        print(
+            format_segment(
+                data_names[i],
+                good.data_segs[i].begin,
+                bad.data_segs[i].begin,
+                sizes[0],
+                sizes[1],
+                tag,
+            )
+        )
+    # TODO: Add diff'ing
+
+    print("[DOL] Oof: Output doesn't match.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--reference", type=Path, required=True, help="Path to reference main.dol"
+    )
+    parser.add_argument(
+        "--target", type=Path, required=True, help="Path to target main.dol"
+    )
+    args = parser.parse_args()
+
+    verify_dol(args.reference, args.target)
