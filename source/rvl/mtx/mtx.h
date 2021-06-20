bdb1615dcb466a0006803964c065421a8e3381fe
Richard Patel <me@terorie.dev>
2021-06-20 23:59:40 +0200

    Decompile RVL/MTX (#19)
    * add PSMTXIdentity
    
    * add PSMTXCopy
    
    * add PSMTXConcat
    
    * add PSMTXConcatArray
    
    * add PSMTXInverse
    
    * add PSMTXInvXpose
    
    * add C_MTXPerspective
    
    * add C_MTXOrtho
    
    * add PSVECAdd
    
    * add PSVECScale
    
    * add PSVECNormalize
    
    * add PSVECMag
    
    * add PSVECDotProduct
    
    * add PSVECCrossProduct
    
    * add C_VECHalfAngle
    
    * add PSVECSquareDistance
    
    * add PSQUATMultiply
    
    * add PSQUATScale
    
    * add PSQUATDotProduct, PSQUOATNormalize
    
    * add PSQUATInverse
    
    * add C_QUATMtx
    
    * add C_QUATLerp
    
    * add PSMTXRotRad, PSMTXRotTrig
    
    * revert RVL_OPTS
    
    * fix relative reference to sdata2
    
    * remove unused function
    
    * add PSMTXRotAxisRad
    
    * add PSMTXTrans
    
    * add PSMTXTransApply
    
    * add PSMTXScale, PSMTXScaleApply
    
    * add PSMTXQuat
    
    * add C_MTXLookAt
    
    * add C_MTXLightFrustum
    
    * add C_MTXLightPerspective
    
    * add C_MTXLightOrtho
    
    * add PSMTXMultVec
    
    * add PSMTXMultVecSR
    
    * add C_MTXFrustum

--- source/rvl/mtx/mtx.h
+++ source/rvl/mtx/mtx.h
@@ -13,7 +13,99 @@ typedef struct {
 typedef f32 Mtx[3][4];
 typedef f32 (*MtxPtr)[4];
 
-void PSMTXRotAxisRad(Mtx m, const Vec* axis, f32 rad);
+typedef f32 Mtx44[4][4];
+typedef f32 (*Mtx44Ptr)[4];
+
+typedef struct {
+  f32 x, y, z, w;
+} Quaternion;
+
+// rvlMtx.c
+// PAL: 0x80199d04
+void PSMTXIdentity(Mtx);
+// PAL: 0x80199d30
+void PSMTXCopy(const Mtx, Mtx);
+// PAL: 0x80199d64
+void PSMTXConcat(const Mtx, const Mtx, Mtx);
+// PAL: 0x80199e30
+void PSMTXConcatArray(const Mtx, const Mtx*, Mtx*, u32);
+// PAL: 0x80199fc8
+u32 PSMTXInverse(const Mtx, Mtx);
+// PAL: 0x8019a0c0
+u32 PSMTXInvXpose(const Mtx, Mtx);
+// PAL: 0x8019a188
+void PSMTXRotRad(Mtx, char, f32);
+// PAL: 0x8019a204
+void PSMTXRotTrig(Mtx, char, f32, f32);
+// PAL: 0x8019a2b4
+void __PSMTXRotAxisRadInternal(Mtx, const Vec*, f32, f32);
+// PAL: 0x8019a364
+void PSMTXRotAxisRad(Mtx, const Vec*, f32);
+// PAL: 0x8019a3e0
+void PSMTXTrans(Mtx, f32, f32, f32);
+// PAL: 0x8019a414
+void PSMTXTransApply(const Mtx, Mtx, f32, f32, f32);
+// PAL: 0x8019a460
+void PSMTXScale(Mtx, f32, f32, f32);
+// PAL: 0x8019a488
+void PSMTXScaleApply(const Mtx, Mtx, f32, f32, f32);
+// PAL: 0x8019a4e0
+void PSMTXQuat(Mtx m, const Quaternion*);
+// PAL: 0x8019a584
+void C_MTXLookAt(Mtx, const Vec*, const Vec*, const Vec*);
+// PAL: 0x8019a6f8
+void C_MTXLightFrustum(Mtx, f32, f32, f32, f32, f32, f32, f32, f32, f32);
+// PAL: 0x8019a79c
+void C_MTXLightPerspective(Mtx, f32, f32, f32, f32, f32, f32);
+// PAL: 0x8019a894
+void C_MTXLightOrtho(Mtx, f32, f32, f32, f32, f32, f32, f32, f32);
+// PAL: 0x8019a91c
+void PSMTXMultVec(const Mtx, const Vec*, Vec*);
+// PAL: 0x8019a970
+void PSMTXMultVecSR(const Mtx, const Vec*, Vec*);
+
+// rvlMtx2.c
+// PAL: 0x8019a9c4
+void C_MTXFrustum(Mtx44, f32, f32, f32, f32, f32, f32);
+// PAL: 0x8019aa60
+void C_MTXPerspective(Mtx44, f32, f32, f32, f32);
+// PAL: 0x8019ab4c
+void C_MTXOrtho(Mtx44, f32, f32, f32, f32, f32, f32);
+
+// rvlVec.c
+// PAL: 0x8019abe4
+void PSVECAdd(const Vec*, const Vec*, Vec*);
+// PAL: 0x8019ac08
+void PSVECScale(const Vec*, Vec*, f32);
+// PAL: 0x8019ac24
+void PSVECNormalize(const Vec*, Vec*);
+// PAL: 0x8019ac68
+f32 PSVECMag(const Vec*);
+// PAL: 0x8019acac
+f32 PSVECDotProduct(const Vec*, const Vec*);
+// PAL: 0x8019accc
+void PSVECCrossProduct(const Vec*, const Vec*, Vec*);
+// PAL: 0x8019ad08
+void C_VECHalfAngle(const Vec*, const Vec*, Vec*);
+// PAL: 0x8019ade0
+f32 PSVECSquareDistance(const Vec*, const Vec*);
+
+// rvlQuat.c
+// PAL: 0x8019ae08
+void PSQUATMultiply(const Quaternion*, const Quaternion*, Quaternion*);
+// PAL: 0x8019ae64
+void PSQUATScale(const Quaternion*, Quaternion*, f32);
+// PAL: 0x8019ae80
+f32 PSQUATDotProduct(const Quaternion*, const Quaternion*);
+// PAL: 0x8019aea0
+void PSQUATNormalize(const Quaternion*, Quaternion*);
+// PAL: 0x8019aef4
+void PSQUATInverse(const Quaternion*, Quaternion*);
+// PAL: 0x8019af48
+void C_QUATMtx(Quaternion*, const Mtx);
+// PAL: 0x8019b114
+void C_QUATLerp(const Quaternion*, const Quaternion*, Quaternion*, f32);
+// PAL: 0x8019b178
 
 #ifdef __cplusplus
 }
---
+++ source/rvl/mtx/rvlQuat.c
@@ -0,0 +1,178 @@
+#include "mtx.h"
+
+void PSQUATMultiply(const register Quaternion* quat1,
+                    const register Quaternion* quat2,
+                    register Quaternion* out) {
+  register f32 vv1, vv2, vv3, vv4;
+  register f32 vv5, vv6, vv7, vv8;
+  register f32 vv9, vvA, vvB, vvC;
+  asm
+  {
+    psq_l vv1, 0(quat1), 0, 0;
+    psq_l vv2, 8(quat1), 0, 0;
+    psq_l vv3, 0(quat2), 0, 0;
+    ps_neg vv7, vv1;
+    psq_l vv4, 8(quat2), 0, 0;
+    ps_neg vv8, vv2;
+    ps_merge01 vv5, vv7, vv1;
+    ps_muls0 vv9, vv2, vv3;
+    ps_muls0 vvA, vv7, vv3;
+    ps_merge01 vv6, vv8, vv2;
+    ps_muls1 vvC, vv5, vv3;
+    ps_madds0 vv9, vv5, vv4, vv9;
+    ps_muls1 vvB, vv6, vv3;
+    ps_madds0 vvA, vv6, vv4, vvA;
+    ps_madds1 vvC, vv8, vv4, vvC;
+    ps_merge10 vv9, vv9, vv9;
+    ps_madds1 vvB, vv1, vv4, vvB;
+    ps_merge10 vvA, vvA, vvA;
+    ps_add vv9, vv9, vvB;
+    psq_st vv9, 0(out), 0, 0;
+    ps_sub vvA, vvA, vvC;
+    psq_st vvA, 8(out), 0, 0;
+  }
+}
+
+void PSQUATScale(const register Quaternion* quat1, register Quaternion* quat2,
+                 register f32 ff1) {
+  register f32 vv1, vv2;
+  asm
+  {
+    psq_l    vv1, 0(quat1), 0, 0;
+    psq_l    vv2, 8(quat1), 0, 0;
+    ps_muls0 vv1, vv1, ff1;
+    psq_st   vv1, 0(quat2), 0, 0;
+    ps_muls0 vv2, vv2, ff1;
+    psq_st   vv2, 8(quat2), 0, 0;
+  }
+}
+
+f32 PSQUATDotProduct(const register Quaternion* quat1,
+                     const register Quaternion* quat2) {
+  register f32 vv1, vv2, vv3, vv4, out;
+  asm
+  {
+    psq_l vv1, 0(quat1), 0, 0;
+    psq_l vv3, 0(quat2), 0, 0;
+    ps_mul out, vv1, vv3;
+    psq_l vv2, 8(quat1), 0, 0;
+    psq_l vv4, 8(quat2), 0, 0;
+    ps_madd out, vv2, vv4, out;
+    ps_sum0 out, out, out, out;
+  }
+  return out;
+}
+
+void PSQUATNormalize(const register Quaternion* src,
+                     register Quaternion* unit) {
+  // sdata2
+  (void)0.00001f;
+  (void)1.0f;
+  (void)0.0f;
+  (void)0.5f;
+  (void)3.0f;
+
+  register f32 vv1, vv2, vv3;
+  register f32 vv4, vv5, vv6;
+  register f32 vv7, vv8;
+  register f32 vv9 = 0.00001f;
+  register f32 vvA = 0.5F;
+  register f32 vvB = 3.0F;
+  asm
+  {
+    psq_l    vv1, 0(src), 0, 0;
+    ps_mul   vv3, vv1, vv1;
+    psq_l    vv2, 8(src), 0, 0;
+    ps_sub   vv6, vv9, vv9;
+    ps_madd  vv3, vv2, vv2, vv3;
+    ps_sum0  vv3, vv3, vv3, vv3;
+    frsqrte  vv4, vv3;
+    ps_sub   vv5, vv3, vv9;
+    fmul     vv7, vv4, vv4;
+    fmul     vv8, vv4, vvA;
+    fnmsub   vv7, vv7, vv3, vvB;
+    fmul     vv4, vv7, vv8;
+    ps_sel   vv4, vv5, vv4, vv6;
+    ps_muls0 vv1, vv1, vv4;
+    ps_muls0 vv2, vv2, vv4;
+    psq_st   vv1, 0(unit), 0, 0;
+    psq_st   vv2, 8(unit), 0, 0;
+  }
+}
+
+void PSQUATInverse(const register Quaternion* src, register Quaternion* inv) {
+  register f32 vv1, vv2, vv3, vv4;
+  register f32 vv5, vv6, vv7, vv8, vv9, vvA, vvB;
+  register f32 vvC = 1.0F;
+  asm {
+    psq_l       vv1, 0(src), 0, 0;
+    ps_mul      vv5, vv1, vv1;
+    ps_sub      vvB, vvC, vvC;
+    psq_l       vv2, 8(src), 0, 0;
+    ps_madd     vv5, vv2, vv2, vv5;
+    ps_add      vvA, vvC, vvC;
+    ps_sum0     vv5, vv5, vv5, vv5;
+    fcmpu       cr0, vv5, vvB;
+    beq-        loc0;
+    fres        vv7, vv5;
+    ps_neg      vv6, vv5;
+    ps_nmsub    vv9, vv5, vv7, vvA;
+    ps_mul      vv7, vv7, vv9;
+    b           loc1;
+loc0:
+    fmr         vv7, vvC;
+loc1:
+    ps_neg      vv8, vv7;
+    ps_muls1    vv4, vv7, vv2;
+    ps_muls0    vv1, vv1, vv8;
+    psq_st      vv4, 12(inv), 1, 0;
+    ps_muls0    vv3, vv2, vv8;
+    psq_st      vv1, 0(inv), 0, 0;
+    psq_st      vv3, 8(inv), 1, 0;
+  }
+}
+
+double sqrt(double);
+inline float sqrtf(float x) { return (float)sqrt(x); }
+
+void C_QUATMtx(Quaternion* r, const Mtx m) {
+  f32 vv0, vv1;
+  s32 i, j, k;
+  s32 idx[3] = {1, 2, 0};
+  f32 vec[3];
+  vv0 = m[0][0] + m[1][1] + m[2][2];
+  if (vv0 > 0.0f) {
+    vv1 = (f32)sqrtf(vv0 + 1.0f);
+    r->w = vv1 * 0.5f;
+    vv1 = 0.5f / vv1;
+    r->x = (m[2][1] - m[1][2]) * vv1;
+    r->y = (m[0][2] - m[2][0]) * vv1;
+    r->z = (m[1][0] - m[0][1]) * vv1;
+  } else {
+    i = 0;
+    if (m[1][1] > m[0][0])
+      i = 1;
+    if (m[2][2] > m[i][i])
+      i = 2;
+    j = idx[i];
+    k = idx[j];
+    vv1 = (f32)sqrtf((m[i][i] - (m[j][j] + m[k][k])) + 1.0f);
+    vec[i] = vv1 * 0.5f;
+    if (vv1 != 0.0f)
+      vv1 = 0.5f / vv1;
+    r->w = (m[k][j] - m[j][k]) * vv1;
+    vec[j] = (m[i][j] + m[j][i]) * vv1;
+    vec[k] = (m[i][k] + m[k][i]) * vv1;
+    r->x = vec[0];
+    r->y = vec[1];
+    r->z = vec[2];
+  }
+}
+
+void C_QUATLerp(const Quaternion* quat1, const Quaternion* quat2,
+                Quaternion* out, f32 f) {
+  out->x = f * (quat2->x - quat1->x) + quat1->x;
+  out->y = f * (quat2->y - quat1->y) + quat1->y;
+  out->z = f * (quat2->z - quat1->z) + quat1->z;
+  out->w = f * (quat2->w - quat1->w) + quat1->w;
+}
