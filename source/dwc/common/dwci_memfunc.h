43194a365c6d299a73dc64a0f19d68aa3c605256
riidefi <34194588+riidefi@users.noreply.github.com>
2021-07-03 20:12:39 -0600

    Start dwc_init.c, dwc_memfunc.c

---
+++ source/dwc/common/dwci_memfunc.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <DWC/common/dwc_memfunc.h>
+#include <types.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define DWCi_MEMFUNC_SIGNATURE 'DWCM'
+
+typedef struct
+{
+	u32 magic; // 00 DWCi_MEMFUNC_SIGNATURE
+	u32 size;  // 04
+	u32 _[6];  // 08
+} DWCi_AllocateHeader;
+
+DWCi_AllocateHeader* DWCi_GetAllocateHeader(void* block);
+
+void* DWCi_SetAllocateHeader(DWCi_AllocateHeader* blockHeader, u32 size);
+
+u32 DWCi_GetAllocateSize(void* block);
+
+void DWCi_SetMemFunc(DWCAllocEx allocator, DWCFreeEx freer);
+
+void* DWCi_GsMalloc(u32 size);
+void* DWCi_GsRealloc(void* block, u32 size);
+void* DWCi_GsFree(void* block);
+void  DWCi_GsMemalign(int align, u32 size);
+
+#ifdef __cplusplus
+}
+#endif
