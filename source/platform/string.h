76941375c0701b65b78fd0cf7d9e6267a1618b02
Richard Patel <me@terorie.dev>
2021-07-04 02:03:26 +0200

    TRK memcpy, memset (#22)

--- source/platform/string.h
+++ source/platform/string.h
@@ -6,7 +6,10 @@
 extern "C" {
 #endif
 
-void memset(void*, s32, u32);
+// PAL: 0x80005f34
+void* memcpy(void*, const void*, u32);
+// PAL: 0x80006038
+void* memset(void*, s32, u32);
 
 #ifdef __cplusplus
 } // extern "C"
