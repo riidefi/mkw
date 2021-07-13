daf578e86aa7f31355a90c400c5169140e06f5f0
CIA gangstalker <iguana9999@protonmail.com>
2021-07-13 05:47:40 +0200

    rvl/tpl: fix code style

--- source/rvl/tpl/tpl.c
+++ source/rvl/tpl/tpl.c
@@ -4,62 +4,49 @@
 #include <rvl/os/os.h>
 
 void TPLBind(TPLPalette* pal) {
-  u16 i;
-
   if (pal->version != 2142000)
     OSPanic("TPL.c", 0x19, "invalid version number for texture palette");
 
-  pal->descriptorArray =
-      (TPLDescriptor*)(((u32)(pal->descriptorArray)) + ((u32)pal));
+  pal->descriptors =
+      (TPLDescriptor*)(((u32)(pal->descriptors)) + ((u32)pal));
 
-  for (i = 0; i < pal->numDescriptors; i++) {
-    if (pal->descriptorArray[i].textureHeader) {
-      pal->descriptorArray[i].textureHeader =
-          (TPLHeader*)(((u32)(pal->descriptorArray[i].textureHeader)) +
+  for (u16 i = 0; i < pal->num_descriptors; i++) {
+    if (pal->descriptors[i].header) {
+      pal->descriptors[i].header =
+          (TPLHeader*)(((u32)(pal->descriptors[i].header)) +
                        ((u32)pal));
-
-      if (!(pal->descriptorArray[i].textureHeader->unpacked)) {
-        pal->descriptorArray[i].textureHeader->data =
-            (void*)((u32)(pal->descriptorArray[i].textureHeader->data) +
+      if (!(pal->descriptors[i].header->unpacked)) {
+        pal->descriptors[i].header->data =
+            (void*)((u32)(pal->descriptors[i].header->data) +
                     (u32)pal);
-        pal->descriptorArray[i].textureHeader->unpacked = 1;
+        pal->descriptors[i].header->unpacked = 1;
       }
     }
-
-    if (pal->descriptorArray[i].CLUTHeader) {
-      pal->descriptorArray[i].CLUTHeader =
-          (TPLClutHeader*)((u32)(pal->descriptorArray[i].CLUTHeader) +
+    if (pal->descriptors[i].clut_header) {
+      pal->descriptors[i].clut_header =
+          (TPLClutHeader*)((u32)(pal->descriptors[i].clut_header) +
                            (u32)pal);
-
-      if (!(pal->descriptorArray[i].CLUTHeader->unpacked)) {
-        pal->descriptorArray[i].CLUTHeader->data =
-            (void*)((u32)(pal->descriptorArray[i].CLUTHeader->data) + (u32)pal);
-        pal->descriptorArray[i].CLUTHeader->unpacked = 1;
+      if (!(pal->descriptors[i].clut_header->unpacked)) {
+        pal->descriptors[i].clut_header->data =
+            (void*)((u32)(pal->descriptors[i].clut_header->data) + (u32)pal);
+        pal->descriptors[i].clut_header->unpacked = 1;
       }
     }
   }
 }
 
 TPLDescriptor* TPLGet(TPLPalette* pal, u32 id) {
-  id %= pal->numDescriptors;
-  return &pal->descriptorArray[id];
+  return &pal->descriptors[id % pal->num_descriptors];
 }
 
 void TPLGetGXTexObjFromPalette(TPLPalette* pal, GXTexObj* to, u32 id) {
-  TPLDescriptor* tdp = TPLGet(pal, id);
-  int mipMap;
-
-  if (tdp->textureHeader->minLOD == tdp->textureHeader->maxLOD)
-    mipMap = 0;
-  else
-    mipMap = 1;
-
-  GXInitTexObj(to, tdp->textureHeader->data, tdp->textureHeader->width,
-               tdp->textureHeader->height, tdp->textureHeader->format,
-               tdp->textureHeader->wrapS, tdp->textureHeader->wrapT, mipMap);
-
-  GXInitTexObjLOD(to, tdp->textureHeader->minFilter,
-                  tdp->textureHeader->magFilter, tdp->textureHeader->minLOD,
-                  tdp->textureHeader->maxLOD, tdp->textureHeader->LODBias, 0,
-                  tdp->textureHeader->edgeLODEnable, 0);
+  TPLDescriptor* desc = TPLGet(pal, id);
+  int mipMap = desc->header->lod_min == desc->header->lod_max ? 0 : 1;
+  GXInitTexObj(to, desc->header->data, desc->header->width,
+               desc->header->height, desc->header->format,
+               desc->header->wrap_s, desc->header->wrap_t, mipMap);
+  GXInitTexObjLOD(to, desc->header->filter_min,
+                  desc->header->filter_mag, desc->header->lod_min,
+                  desc->header->lod_max, desc->header->lod_bias, 0,
+                  desc->header->edge_lod_enabled, 0);
 }
--- source/rvl/tpl/tpl.h
+++ source/rvl/tpl/tpl.h
@@ -9,7 +9,7 @@ extern "C" {
 typedef struct {
   u16 numEntries;
   u8 unpacked;
-  u8 pad8;
+  u8 _;
   int format;
   void* data;
 } TPLClutHeader;
@@ -19,26 +19,26 @@ typedef struct {
   u16 width;
   u32 format;
   void* data;
-  int wrapS;
-  int wrapT;
-  int minFilter;
-  int magFilter;
-  float LODBias;
-  u8 edgeLODEnable;
-  u8 minLOD;
-  u8 maxLOD;
+  int wrap_s;
+  int wrap_t;
+  int filter_min;
+  int filter_mag;
+  float lod_bias;
+  u8 edge_lod_enabled;
+  u8 lod_min;
+  u8 lod_max;
   u8 unpacked;
 } TPLHeader;
 
 typedef struct {
-  TPLHeader* textureHeader;
-  TPLClutHeader* CLUTHeader;
+  TPLHeader* header;
+  TPLClutHeader* clut_header;
 } TPLDescriptor;
 
 typedef struct {
   u32 version;
-  u32 numDescriptors;
-  TPLDescriptor* descriptorArray;
+  u32 num_descriptors;
+  TPLDescriptor* descriptors;
 } TPLPalette;
 
 void TPLBind(TPLPalette*);
