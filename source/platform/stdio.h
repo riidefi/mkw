a6eac70be7ae2f8f294637b4a8098a0fe6cdabdf
Richard Patel <me@terorie.dev>
2021-07-22 23:40:58 +0200

    Clean up RVL/NAND (#49)
    * Clean up nand.c
    
    * lol
    
    * Disable clang-format in inline asm
    
    * lol

--- source/platform/stdio.h
+++ source/platform/stdio.h
@@ -64,8 +64,11 @@ struct _FILE {
 #define SEEK_END 2
 #define SEEK_SET 0
 
+typedef char* va_list;
+
 int sprintf(char* str, const char* format, ...);
 int snprintf(char* s, size_t n, const char* format, ...);
+int vsnprintf(char* s, size_t n, const char* format, va_list arg);
 
 int sscanf(const char* str, const char* format, ...);
 
