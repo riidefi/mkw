86aaeae1a4965a125548b0f5e34076d0f7392a26
Richard Patel <me@terorie.dev>
2021-08-01 03:20:55 +0200

    add trk/start.c

--- sources.py
+++ sources.py
@@ -28,6 +28,7 @@ class Source:
 
 SOURCES_TRK = [
     Source(src="source/rvl/trk/rvlTrkMem.c", cc='4199_60831', opts=RVL_OPTS),
+    Source(src="source/rvl/trk/start.c", cc='4199_60831', opts=RVL_OPTS),
 ]
 SOURCES_MSL_LIBC = [
     Source(src="source/platform/wchar.c", cc='4201_127', opts=RVL_OPTS),
