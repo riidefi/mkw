0c1276656a0b5c1f7ae038fd302b778dfb6ad562
Pablo Stebler <pablo@stebler.xyz>
2021-09-02 23:15:03 +0200

    Disable section declspec for non-mwcc

--- source/platform/__ppc_eabi_linker.h
+++ source/platform/__ppc_eabi_linker.h
@@ -10,4 +10,6 @@ typedef struct __eti_init_info {
 // _eti_init_info is a linker-generated symbol.
 // Since we don't have proper extab/extabindex sections,
 // trying to reference it is going to fail.
+#ifdef __CWCC__
 __declspec(section ".init") extern __eti_init_info _eti_init_info_[];
+#endif
