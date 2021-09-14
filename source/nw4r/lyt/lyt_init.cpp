b39e951811951d7e0d0d282c8ff0b980c78804e4
riidefi <34194588+riidefi@users.noreply.github.com>
2021-09-13 21:57:09 -0600

    [UI] clang-format, fix typo

--- source/nw4r/lyt/lyt_init.cpp
+++ source/nw4r/lyt/lyt_init.cpp
@@ -6,7 +6,7 @@ namespace nw4r {
 namespace lyt {
 
 static const char* NW4R_LYT_VERSION_STRING =
-  "<< NW4R    - LYT 	final   build: Mar  8 2008 20:59:41 (0x4201_127) >>";
+    "<< NW4R    - LYT 	final   build: Mar  8 2008 20:59:41 (0x4201_127) >>";
 
 void LytInit() {
   OSRegisterVersion(NW4R_LYT_VERSION_STRING);
