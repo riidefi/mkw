ee044ac84c3788860719188bcdd08ecb4296bed9
Richard Patel <me@terorie.dev>
2021-09-26 23:59:54 +0200

    Split eggQuat (#120)
    * Split eggQuat
    
    * revert changes to flags
    
    * fix symbols.txt
    
    * replace pointers with consts
    
    * fix Quat usage
    
    * fix norm
    
    * fix inverse
    
    * clang-format
    
    * Update eggQuat.hpp
    
    * Update eggQuat.cpp
    
    * Update dol.lcf.j2
    
    * Update symbols.txt

--- source/egg/math/eggQuat.hpp
+++ source/egg/math/eggQuat.hpp
@@ -7,13 +7,27 @@
 
 #include <rk_types.h>
 
+#include "eggVector.hpp"
+
 namespace EGG {
 
 class Quatf {
 public:
   void set(float a, float b, float c, float d);
+  void setRPY(const Vector3f& euler);
+  void setRPY(float r, float p, float y);
+  void setAxisRotation(const Vector3f& axis, float angle);
+  float squareNorm();
+  void normalise();
+  Quatf inverse();
+  void rotateVector(const Vector3f& v);
+  void rotateVectorInv(const Vector3f& v);
+  void slerpTo(const Quatf& r4, float, Quatf& r5) const;
+  void makeVectorRotation(Vector3f& v0, Vector3f& v1);
 
   f32 _[4];
 };
+
+Quatf operator*(const Quatf&, const Vector3f&);
 
 } // namespace EGG
