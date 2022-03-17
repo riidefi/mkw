1d653b77c8656cc831eec4042cf2fec6a42e76d7
Richard Patel <me@terorie.dev>
2022-03-17 23:59:37 +0100

    more dvdSoundArchive

---
+++ source/nw4r/snd/snd_soundArchiveFile.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <rk_types.h>
+#include <decomp.h>
+
+namespace nw4r {
+namespace snd {
+namespace detail {
+
+class SoundArchiveFileReader {
+
+public:
+  SoundArchiveFileReader();
+
+private:
+  char _unk00[40];
+  void* _unk28;
+  void* _unk2c;
+  void* _unk30;
+  void* _unk34;
+  void* _unk38;
+  void* _unk3c;
+  void* _unk40;
+};
+
+} // namespace detail
+} // namespace snd
+} // namespace nw4r
