eedeee6e8e181c22f01a8586623172384d44f604
riidefi <34194588+riidefi@users.noreply.github.com>
2022-03-24 07:22:37 -0600

    Split ut_LinkList

---
+++ source/nw4r/ut/ut_LinkList.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x800af210..0x800af2a0
+UNKNOWN_FUNCTION(__dt__Q44nw4r2ut6detail12LinkListImplFv);
+// PAL: 0x800af2a0..0x800af2f0
+UNKNOWN_FUNCTION(
+    Erase__Q44nw4r2ut6detail12LinkListImplFQ54nw4r2ut6detail12LinkListImpl8Iterator);
+// PAL: 0x800af2f0..0x800af340
+UNKNOWN_FUNCTION(Clear__Q44nw4r2ut6detail12LinkListImplFv);
+// PAL: 0x800af340..0x800af370
+UNKNOWN_FUNCTION(
+    Insert__Q44nw4r2ut6detail12LinkListImplFQ54nw4r2ut6detail12LinkListImpl8IteratorPQ34nw4r2ut12LinkListNode);
+// PAL: 0x800af370..0x800af3a0
+UNKNOWN_FUNCTION(
+    Erase__Q44nw4r2ut6detail12LinkListImplFPQ34nw4r2ut12LinkListNode);
+
+#ifdef __cplusplus
+}
+#endif
