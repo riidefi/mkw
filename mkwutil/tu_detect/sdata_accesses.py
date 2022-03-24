b2cf4744cff751ae27b116b58568440d10b2c938
Richard Patel <richard@blockdaemon.com>
2022-03-24 12:59:33 +0100

    tu_detect/sdata_accesses: improved xref detection

--- mkwutil/tu_detect/sdata_accesses.py
+++ mkwutil/tu_detect/sdata_accesses.py
@@ -1,14 +1,16 @@
 """
-Attempts to detect TU boundaries by looking at zero byte padding between functions.
+Attempts to detect TU boundaries by looking at sdata access patterns.
 """
 
 import argparse
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+from re import I
 import struct
+import sys
 from typing import Optional, Iterator
 
-from mkwutil.lib.dol import DolSegment
+from mkwutil.lib.dol import DolBinary, DolSegment
 from mkwutil.project import read_dol
 
 
@@ -16,9 +18,59 @@ from mkwutil.project import read_dol
 class SdataAccess:
     at: int
     target: int
+    size: int = 4
+    ps_ext: bool = False
 
     def __str__(self) -> str:
         return "%08x %08x" % (self.target, self.at)
+
+
+@dataclass
+class SdataSite:
+    at: int
+    refs: set[int] = field(default_factory=lambda: set())
+    value: Optional[int] = None
+    size: Optional[int] = None
+    ps_ext: bool = False
+
+    def add(self, access: SdataAccess):
+        if self.size is not None and self.size != access.size:
+            print("WARN: access size mismatch", file=sys.stderr)
+            self.size = 4
+        else:
+            self.size = access.size
+        self.refs.add(access.at)
+        self.ps_ext = self.ps_ext or access.ps_ext
+
+    def read_from(self, bin: DolBinary):
+        if self.size == 8:
+            self.value = bin.virtual_read_dword(self.at)
+        else:
+            self.value = bin.virtual_read_word(self.at)
+
+    def __str__(self) -> str:
+        if self.value is None:
+            value_str = "????????"
+        elif self.size == 8:
+            value_str = "%016x" % (self.value)
+        else:
+            value_str = "%08x" % (self.value) + " " * 8
+        if len(self.refs) > 0:
+            ret = "%08x %s % 4d %08x..%08x" % (
+                self.at,
+                value_str,
+                len(self.refs),
+                min(self.refs),
+                max(self.refs),
+            )
+        else:
+            ret = "%08x %s" % (self.at, value_str)
+        if self.ps_ext:
+            ret += " (psq)"
+        return ret
+
+    def __lt__(self, other) -> bool:
+        return self.at < other.at
 
 
 class SdataAccessScanner:
@@ -42,8 +94,18 @@ class SdataAccessScanner:
         blob = self.segment.virtual_read(at, 4)
         word = struct.unpack(">I", blob)[0]
 
-        # lwz opcode
-        if word >> 26 != 32:
+        ps_ext = False
+        if word >> 26 in (32, 34, 36, 38, 40, 44, 48, 52):
+            # lwz, lbz, stw, stb, lhz, sth, lfs, stfs opcodes
+            access_size = 4
+        elif word >> 26 in (50, 54):
+            # lfd, stfd opcodes
+            access_size = 8
+        elif word >> 26 in (56, 60):
+            # psq_l, psq_st
+            access_size = 4
+            ps_ext = True
+        else:
             return
         # r2 or r13
         rT = word >> 16 & 0b11111
@@ -53,11 +115,25 @@ class SdataAccessScanner:
             base = SdataAccessScanner.R13
         else:
             return
-        offset_uimm = word & 0xFFFF
-        offset = struct.unpack(">h", struct.pack(">H", (offset_uimm)))[0]
+        # offset
+        if not ps_ext:
+            offset_uimm = word & 0xFFFF
+            offset = struct.unpack(">h", struct.pack(">H", (offset_uimm)))[0]
+            offset = offset & ~3
+        else:
+            offset_uimm = word & 0x1FFF
+            offset = offset & ~3
 
-        access = SdataAccess(at, base + offset)
-        print(access)
+        return SdataAccess(at, base + offset, size=access_size, ps_ext=ps_ext)
+
+
+class SdataAccessIndexer:
+    def __init__(self):
+        self.sites = dict()
+
+    def add(self, access: SdataAccess):
+        site: SdataSite = self.sites.setdefault(access.target, SdataSite(access.target))
+        site.add(access)
 
 
 def main():
@@ -69,7 +145,22 @@ def main():
     text_segment = dol.segments[DolSegment.NAMES.index("text")]
 
     scanner = SdataAccessScanner(text_segment)
-    list(scanner.scan())
+    indexer = SdataAccessIndexer()
+    for access in scanner.scan():
+        indexer.add(access)
+    last = None
+    for site in sorted(indexer.sites.values()):
+        if site.at >= 0x80389140:
+            break
+        site.read_from(dol)
+        if last is None:
+            last = site.at
+        while last < site.at:
+            assert last % 4 == 0
+            last += 4
+            if last + (site.size or 4) < site.at:
+                print(SdataSite(at=last, value=dol.virtual_read_word(last)))
+        print(site)
 
 
 if __name__ == "__main__":
