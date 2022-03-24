62d9fec159f15d0fd163cda27a3f00394a63657e
Richard Patel <richard@blockdaemon.com>
2022-03-24 13:34:35 +0100

    tu_detect/sdata_accesses: sdata2/text TU detection

--- mkwutil/tu_detect/sdata_accesses.py
+++ mkwutil/tu_detect/sdata_accesses.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from re import I
 import struct
 import sys
-from typing import Optional, Iterator
+from typing import Optional, Iterable, Iterator
 
 from mkwutil.lib.dol import DolBinary, DolSegment
 from mkwutil.project import read_dol
@@ -135,6 +135,70 @@ class SdataAccessIndexer:
         site: SdataSite = self.sites.setdefault(access.target, SdataSite(access.target))
         site.add(access)
 
+    def linear_pass(self, dol: DolBinary) -> Iterator[SdataAccess]:
+        last = None
+        for site in sorted(self.sites.values()):
+            if site.at >= 0x80389140:
+                break
+            site.read_from(dol)
+            if last is None:
+                last = site.at
+            while last < site.at:
+                assert last % 4 == 0
+                last += 4
+                if last + (site.size or 4) < site.at:
+                    yield SdataSite(at=last, value=dol.virtual_read_word(last))
+            yield site
+
+
+class Sdata2TUDetector:
+    def __init__(self, sites: Iterable[SdataSite]):
+        self.sites = sites
+
+    def _reset_tu(self):
+        self.tu = []
+        self.vals = set()
+        self.found_padding = False
+    
+    def run(self):
+        self._reset_tu()
+        for site in self.sites:
+            if site.at < 0x80386fa0:
+                continue
+            if self.found_padding:
+                self._next_tu()
+            self._next_site(site)
+
+    def tu_spread(sites) -> tuple[int, int]:
+        min_ref = 0xFFFF_FFFF_FFFF_FFFF
+        max_ref = 0
+        for site in sites:
+            for ref in site.refs:
+                min_ref = min(min_ref, ref)
+                max_ref = max(max_ref, ref)
+        return min_ref, max_ref
+
+    def _dump_tu(self):
+        if len(self.tu) == 0:
+            return
+        min_text, max_text = Sdata2TUDetector.tu_spread(self.tu)
+        if max_text == 0:
+            return
+        print("%08x..%08x %08x..%08x" % (self.tu[0].at, self.tu[-1].at, min_text, max_text))
+
+    def _next_tu(self):
+        self._dump_tu()
+        self._reset_tu()
+
+    def _next_site(self, site: SdataSite):
+        if site.value in self.vals:
+            if site.value == 0 and len(site.refs) == 0 and site.at % 8 == 4:
+                self.found_padding = True
+                return
+            self._next_tu()
+        self.vals.add(site.value)
+        self.tu.append(site)
+
 
 def main():
     parser = argparse.ArgumentParser(description="TU detect text padding heuristic")
@@ -144,23 +208,21 @@ def main():
     dol = read_dol(Path("./artifacts/orig/pal/main.dol"))
     text_segment = dol.segments[DolSegment.NAMES.index("text")]
 
+    # Linear pass over text section for xrefs and index them by sdata address.
     scanner = SdataAccessScanner(text_segment)
     indexer = SdataAccessIndexer()
     for access in scanner.scan():
         indexer.add(access)
-    last = None
-    for site in sorted(indexer.sites.values()):
-        if site.at >= 0x80389140:
-            break
-        site.read_from(dol)
-        if last is None:
-            last = site.at
-        while last < site.at:
-            assert last % 4 == 0
-            last += 4
-            if last + (site.size or 4) < site.at:
-                print(SdataSite(at=last, value=dol.virtual_read_word(last)))
+
+    # Linear pass over sdata section.
+    sites = list(indexer.linear_pass(dol))
+    for site in sites:
         print(site)
+    print()
+
+    # sdata2 section detect
+    detector = Sdata2TUDetector(sites)
+    detector.run()
 
 
 if __name__ == "__main__":
