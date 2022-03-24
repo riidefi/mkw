d4884d563b95954c32f89e0a5221e2ba18066658
riidefi <34194588+riidefi@users.noreply.github.com>
2022-03-24 07:58:33 -0600

    Split ut_binaryFileFormat

---
+++ source/nw4r/ut/ut_binaryFileFormat.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x800af3a0..0x800af420
+UNKNOWN_FUNCTION(
+    IsValidBinaryFile__Q24nw4r2utFPCQ34nw4r2ut16BinaryFileHeaderUlUsUs);
+
+#ifdef __cplusplus
+}
+#endif
