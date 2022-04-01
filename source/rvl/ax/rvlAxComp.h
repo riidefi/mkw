e04c6871632dd0ca5fa20fa95593b9146b698ecc
Richard Patel <me@terorie.dev>
2022-04-02 01:24:21 +0200

    split rvl/ax/rvlAxComp

---
+++ source/rvl/ax/rvlAxComp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <rk_types.h>
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x8027e860..0x8027f820
+extern u16 __AXCompressorTable[2016];
+
+#ifdef __cplusplus
+}
+#endif
