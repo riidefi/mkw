5c8a1d9196d524d40e0b337e55b1d0e50f102059
Richard Patel <me@terorie.dev>
2022-04-02 00:30:52 +0200

    split rvl/ax/rvlAxAlloc

---
+++ source/rvl/ax/rvlAxAlloc.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x80124edc..0x80124ef0
+UNKNOWN_FUNCTION(__AXGetStackHead);
+// PAL: 0x80124ef0..0x80124f9c
+UNKNOWN_FUNCTION(__AXServiceCallbackStack);
+// PAL: 0x80124f9c..0x8012504c
+UNKNOWN_FUNCTION(__AXAllocInit);
+// PAL: 0x8012504c..0x80125068
+UNKNOWN_FUNCTION(__AXPushFreeStack);
+// PAL: 0x80125068..0x80125078
+UNKNOWN_FUNCTION(__AXPushCallbackStack);
+// PAL: 0x80125078..0x801250fc
+UNKNOWN_FUNCTION(__AXRemoveFromStack);
+// PAL: 0x801250fc..0x80125178
+UNKNOWN_FUNCTION(AXFreeVoice);
+// PAL: 0x80125178..0x80125394
+UNKNOWN_FUNCTION(AXAcquireVoice);
+
+#ifdef __cplusplus
+}
+#endif
