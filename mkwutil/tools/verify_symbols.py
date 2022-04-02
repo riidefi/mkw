8568ecaaf9bc502d8a3fac7371a0230184c56f22
Richard Patel <me@terorie.dev>
2022-04-03 01:28:46 +0200

    add data syms to symbols.txt

--- mkwutil/tools/verify_symbols.py
+++ mkwutil/tools/verify_symbols.py
@@ -16,23 +16,30 @@ from mkwutil.project import read_symbol_map
 
 def diff_symbols(elf_sym: Symbol, orig_sym: Optional[Symbol]) -> bool:
     if orig_sym is None:
-        if not elf_sym.name.startswith("unk_"):
+        if not elf_sym.name.startswith("unk_") and not elf_sym.name.startswith("_unk_"):
             print("[-] 0x%08x %s" % (elf_sym.addr, elf_sym.name))
         return False
+    if elf_sym.name in ("__save_gpr", "__rest_gpr"):
+        return True # ignore
     if elf_sym.name != orig_sym.name:
         print("[!] 0x%08x %s != %s" % (elf_sym.addr, elf_sym.name, orig_sym.name))
+        return False
+    elif elf_sym.addr != orig_sym.addr:
+        print("[!] 0x%08x %s != 0x%08x" % (elf_sym.addr, elf_sym.name, orig_sym.addr))
         return False
     return True
 
 
-def verify_symbols(symbols_txt: Path, target_elf) -> bool:
+def verify_symbols(symbols_txt: Path, target_elf):
     elf = ELFFile(target_elf)
     elf_syms = symbols_from_elf(elf)
     orig_syms = read_symbol_map(symbols_txt)
-    mismatch = True
     for elf_sym in elf_syms:
         orig_sym = orig_syms.get(elf_sym.addr)
-        mismatch &= diff_symbols(elf_sym, orig_sym)
+        diff_symbols(elf_sym, orig_sym)
+        orig_sym = orig_syms.get_by_name(elf_sym.name)
+        if orig_sym:
+            diff_symbols(elf_sym, orig_sym)
 
 
 if __name__ == "__main__":
@@ -49,5 +56,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     with open(args.elf, "rb") as elf_file:
-        if not verify_symbols(args.symbols, elf_file):
-            sys.exit(1)
+        verify_symbols(args.symbols, elf_file)
