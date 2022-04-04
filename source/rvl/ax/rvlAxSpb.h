943d25e1c504dd66621a3bdb77950475b56a436a
Richard Patel <me@terorie.dev>
2022-04-04 15:33:43 +0200

    split rvlAxSpb

---
+++ source/rvl/ax/rvlAxSpb.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <rk_types.h>
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x80126e30..0x80126e3c
+UNKNOWN_FUNCTION(__AXGetStudio);
+// PAL: 0x80126e3c..0x80126ea8
+UNKNOWN_FUNCTION(__AXDepopFadeMain);
+// PAL: 0x80126ea8..0x80126f14
+UNKNOWN_FUNCTION(__AXDepopFadeRmt);
+// PAL: 0x80126f14..0x8012708c
+UNKNOWN_FUNCTION(__AXPrintStudio);
+// PAL: 0x8012708c..0x801270e4
+UNKNOWN_FUNCTION(__AXSPBInit);
+// PAL: 0x801270e4..0x80127250
+UNKNOWN_FUNCTION(__AXDepopVoice);
+
+#ifdef __cplusplus
+}
+#endif
