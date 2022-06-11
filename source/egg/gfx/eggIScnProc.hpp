8fe1a9290288953e5bbfbe252dd44fa30a560d4b
riidefi <34194588+riidefi@users.noreply.github.com>
2022-06-11 09:48:07 -0600

    Split eggIScnProc.cpp

---
+++ source/egg/gfx/eggIScnProc.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x80229fac..0x8022a05c
+UNKNOWN_FUNCTION(unk_80229fac);
+// PAL: 0x8022a05c..0x8022a164
+UNKNOWN_FUNCTION(unk_8022a05c);
+// PAL: 0x8022a164..0x8022a210
+UNKNOWN_FUNCTION(unk_8022a164);
+// PAL: 0x8022a210..0x8022a294
+UNKNOWN_FUNCTION(pushToScnGroup__Q23EGG8IScnProcFPQ34nw4r3g3d8ScnGroup);
+// PAL: 0x8022a294..0x8022a314
+UNKNOWN_FUNCTION(removeFromScnGroup__Q23EGG8IScnProcFPQ34nw4r3g3d8ScnGroup);
+// PAL: 0x8022a314..0x8022a38c
+UNKNOWN_FUNCTION(unk_8022a314);
+
+#ifdef __cplusplus
+}
+#endif
