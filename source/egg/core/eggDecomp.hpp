1d546d29a29074eee0e31cb8b60117f2604e633c
SwareJonge <41187958+SwareJonge@users.noreply.github.com>
2022-06-14 01:38:48 +0200

    Decompile Useful part of eggDecomp (#190)
    * decompile parts of eggDecomp.cpp
    
    * replace inline functions with defines
    
    * fix formatting

--- source/egg/core/eggDecomp.hpp
+++ source/egg/core/eggDecomp.hpp
@@ -5,28 +5,34 @@
 namespace EGG {
 
 namespace Decomp {
+
+enum CompressionType {
+  TYPE_UNKNOWN = 0,
+  TYPE_SZS = 1,
+  TYPE_ASH = 2,
+  TYPE_ASR = 3,
+};
+
 // PAL: 0x80218ba4..0x80218c2c
-void decode(unsigned char*, unsigned char*);
+s32 decode(const u8*, u8*);
 // PAL: 0x80218c2c..0x80218dc0
-void decodeSZS(unsigned char*, unsigned char*);
+s32 decodeSZS(const u8*, u8*);
 // PAL: 0x80218dc0..0x80218ff0
-void decodeASH(unsigned char*, unsigned char*);
+s32 decodeASH(const u8*, u8*);
 // PAL: 0x80218ff0..0x802198f8
-void decodeASR(unsigned char*, unsigned char*);
+s32 decodeASR(const u8*, u8*);
 // PAL: 0x802198f8..0x8021997c
-int checkCompressed(unsigned char*);
+CompressionType checkCompressed(const u8*);
 // PAL: 0x8021997c..0x80219a7c
-int getExpandSize(unsigned char*);
+s32 getExpandSize(const u8*);
 // PAL: 0x80219a7c..0x80219b84
-int getBitsCode(unsigned char*, int, int);
+int getBitsCode(const u8*, int, int);
 // PAL: 0x80219b84..0x80219c10
-int getBit1c(unsigned char*, int);
+int getBit1c(const u8*, int);
 // PAL: 0x80219c10..0x80219d34
-void readTree9(unsigned char*, unsigned short*, unsigned short*,
-               unsigned short*);
+void readTree9(const u8*, unsigned short*, unsigned short*, unsigned short*);
 // PAL: 0x80219d34..0x80219e68
-void readTree12(unsigned char*, unsigned short*, unsigned short*,
-                unsigned short*);
+void readTree12(const u8*, unsigned short*, unsigned short*, unsigned short*);
 } // namespace Decomp
 
 } // namespace EGG
