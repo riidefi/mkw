5452881733f0044f9858f686651c66a2faeb3505
riidefi <34194588+riidefi@users.noreply.github.com>
2022-06-14 10:59:33 -0600

    Split eggCompress.cpp

---
+++ source/egg/core/eggCompress.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x802156ac..0x8021587c
+UNKNOWN_FUNCTION(Comp_encodeSZS);
+// PAL: 0x8021587c..0x802159b8
+UNKNOWN_FUNCTION(unk_8021587c);
+// PAL: 0x802159b8..0x80215aa8
+UNKNOWN_FUNCTION(unk_802159b8);
+// PAL: 0x80215aa8..0x80215cbc
+UNKNOWN_FUNCTION(unk_80215aa8);
+
+#ifdef __cplusplus
+}
+#endif
