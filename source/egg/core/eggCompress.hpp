cc239bc6281bc002d168461e6183651d744cd836
riidefi <34194588+riidefi@users.noreply.github.com>
2022-06-14 21:41:08 -0600

    Decompile eggCompress.cpp (SZS)

--- source/egg/core/eggCompress.hpp
+++ source/egg/core/eggCompress.hpp
@@ -2,21 +2,10 @@
 
 #include <rk_types.h>
 
-#include <decomp.h>
+namespace EGG {
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+//! Boyer-moore-horspool SZS encoder
+//! @returns compressed size
+int encodeSZS(const u8* src, u8* dst, int srcSize);
 
-// PAL: 0x802156ac..0x8021587c
-UNKNOWN_FUNCTION(Comp_encodeSZS);
-// PAL: 0x8021587c..0x802159b8
-UNKNOWN_FUNCTION(unk_8021587c);
-// PAL: 0x802159b8..0x80215aa8
-UNKNOWN_FUNCTION(unk_802159b8);
-// PAL: 0x80215aa8..0x80215cbc
-UNKNOWN_FUNCTION(unk_80215aa8);
-
-#ifdef __cplusplus
-}
-#endif
+} // namespace EGG
