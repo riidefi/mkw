42eb7f92c0d3b66e2ecbcca2c9c7751189d694a4
Aiden <86704247+vabold@users.noreply.github.com>
2022-07-07 13:26:29 -0400

    Decomp: ResourceManager (#192)
    * MenuCharacterManager: full match
    
    Co-Authored-By: riidefi <34194588+riidefi@users.noreply.github.com>
    
    * JobContext: full match
    
    Co-Authored-By: em-eight <107326447+em-eight@users.noreply.github.com>
    
    * CourseCache: ctor, dtor, init
    
    * CourseCache: load
    
    * Add loadCourse WIP
    
    * WIP comment requestLoad ASM
    
    * Match loadCourse and requestLoad
    
    * CourseCache: loadOther, full match
    
    * ResourceManager: createInstance
    
    * ResourceManager: loadMission, loadCompetition
    
    Co-Authored-By: riidefi <34194588+riidefi@users.noreply.github.com>
    Co-Authored-By: em-eight <107326447+em-eight@users.noreply.github.com>
    
    * ResourceManager: doLoadTask
    
    * Dummy: unused vtable calls
    
    * ResourceManager: requestLoad (DvdArchive)
    
    * ResourceManager: requestLoad (MultiDvdArchive, fileHeap)
    
    * ResourceManager: load / rodata
    
    * ResourceManager: rodata formatting
    
    * A couple of more functions
    
    * ResourceManager: loadUI, minor cleanup
    
    * ResourceManager: loadSystemResource
    
    * ResourceManager: loadMenuKartModel, minor cleanup
    
    * ResourceManager: unmountMulti, unmountArchive
    
    * ResourceManager: getFile
    
    * ResourceManager: getCharacterFile
    
    * ResourceManager: getVehicleFile
    
    * ResourceManager: getFileCopy
    
    * a couple of functions
    
    * ResourceManager: cleanup
    
    * ResourceManager: clear
    
    * ResourceManager: getBspFile, CourseCache: load
    
    * ResourceManager: getBspFile, CourseCache: load
    
    * Unused: getNullString
    
    * ResourceManager: setArcResourceLink
    
    * ResourceManager: attachArcResourceAccessor
    
    * ResourceManager: attachArcResourceAccessor
    
    * request things
    
    * doLoadGlobe
    
    * globe loads
    
    * load globe async
    
    * (de)init globeHeap
    
    * WIP: doLoadMenuCharacterModel
    
    * Match doLoadCharacterKartModel
    
    * Match deinitGlobeHeap
    
    * doLoadCharacterModel and staffGhost
    
    * loadCharMdlAsync
    
    * FUN22CC
    
    * ResourceManager: start cleanup
    
    Co-authored-by: em-eight <107326447+em-eight@users.noreply.github.com>
    Co-authored-by: Theodoros Tyrovouzis <teotyrov@gmail.com>
    Co-authored-by: riidefi <34194588+riidefi@users.noreply.github.com>

---
+++ source/game/system/RaceConfig.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <rk_types.h>
+
+struct RaceConfigPlayer {
+  u8 _00[0x08 - 0x00];
+  u32 vehicleId;
+  u32 characterId;
+  u32 type;
+  u8 _14[0xd0 - 0x14];
+  s32 controllerId;
+  u8 _d4[0xf0 - 0xd4];
+};
+
+struct RaceConfigScenario {
+  u8 _000[0x004 - 0x000];
+  u8 playerCount;
+  u8 _005[0x008 - 0x005];
+  RaceConfigPlayer players[12];
+  // ...
+};
+
+struct RaceConfig {
+  static RaceConfig* spInstance;
+  u8 _0000[0x0020 - 0x0000];
+  RaceConfigScenario raceScenario;
+  // ...
+};
