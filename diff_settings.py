7f5bb83a64e0faa71d62f4175910396296f2fe4c
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-07-07 22:30:29 +0100

    diff.py rel support

--- diff_settings.py
+++ diff_settings.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import os
 from pathlib import Path
 
@@ -5,9 +6,14 @@ DEVKITPPC = Path(os.environ.get("DEVKITPPC", "./tools/devkitppc"))
 
 
 def apply(config: dict, args):
-    config["mapfile"] = "artifacts/target/pal/main.map"
-    config["myimg"] = "artifacts/target/pal/main.dol"
-    config["baseimg"] = "artifacts/orig/pal/main.dol"
+    if args.rel:
+        config["mapfile"] = "artifacts/target/pal/staticR.map"
+        config["myimg"] = "artifacts/target/pal/staticR.rel"
+        config["baseimg"] = "artifacts/orig/pal/staticR.rel"
+    else:
+        config["mapfile"] = "artifacts/target/pal/main.map"
+        config["myimg"] = "artifacts/target/pal/main.dol"
+        config["baseimg"] = "artifacts/orig/pal/main.dol"
     config["makeflags"] = []
     config["source_directories"] = ["source"]
     config["arch"] = "ppc"
@@ -15,3 +21,6 @@ def apply(config: dict, args):
     config["mw_build_dir"] = "out"  # only needed for mw map format
     config["makeflags"] = []
     config["objdump_executable"] = DEVKITPPC / "bin" / "powerpc-eabi-objdump.exe"
+
+def add_custom_arguments(parser: ArgumentParser):
+    parser.add_argument("-r", "--rel", action="store_true", help="(MKW) Diff a function in staticR.rel")
