bf3a8c60f7cd010e86eaf19761af877f5c145da2
Seeky <58006653+SeekyCt@users.noreply.github.com>
2022-07-08 16:06:59 +0100

    More diff.py improvements (#196)

--- diff_settings.py
+++ diff_settings.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 import os
 from pathlib import Path
+from sys import executable
 
 DEVKITPPC = Path(os.environ.get("DEVKITPPC", "./tools/devkitppc"))
 
@@ -14,6 +15,7 @@ def apply(config: dict, args):
         config["mapfile"] = "artifacts/target/pal/main.map"
         config["myimg"] = "artifacts/target/pal/main.dol"
         config["baseimg"] = "artifacts/orig/pal/main.dol"
+    config["make_command"] = [executable, "build.py", "--diff_py"]
     config["makeflags"] = []
     config["source_directories"] = ["source"]
     config["arch"] = "ppc"
@@ -21,6 +23,7 @@ def apply(config: dict, args):
     config["mw_build_dir"] = "out"  # only needed for mw map format
     config["makeflags"] = []
     config["objdump_executable"] = DEVKITPPC / "bin" / "powerpc-eabi-objdump.exe"
+    config["show_line_numbers_default"] = True
 
 def add_custom_arguments(parser: ArgumentParser):
     parser.add_argument("-r", "--rel", action="store_true", help="(MKW) Diff a function in staticR.rel")
