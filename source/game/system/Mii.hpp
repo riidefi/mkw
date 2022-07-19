b7adf349901294b908715f412ea01901aeb80f34
vabold <vabold820@gmail.com>
2022-07-19 12:23:49 -0400

    Mii: better infrastructure

--- source/game/system/Mii.hpp
+++ source/game/system/Mii.hpp
@@ -21,6 +21,10 @@ struct unkStruct {
   u8 x3;
 };
 
+struct MiiId {
+  u8 _00[0x8 - 0x0];
+};
+
 class Mii {
 public:
   Mii(s32 num) { init(num); }
@@ -30,14 +34,29 @@ public:
   u8 _8;
   s8 _9;
   s32 _C;
-  unk8 _10[0x60 - 0x10];
-  unkStruct _60;
+  RawMii mRaw;
+  unk32 _5C;
+  unkStruct _60; // almost certainly a color struct
   s32 _64;
-  u8 _68[0xa4 - 0x68];
+  char mName[20];
+  unk32 _7C;
+  unk32 _80;
+  unk32 _84;
+  unk32 _88;
+  unk32 _8C;
+  unk32 _90;
+  MiiId mId;
+  unk32 _9C;
+  unk32 _A0;
   u8 _A4;
   u8 _A5;
   u8 _A6;
-  unk8 _[0xb8 - 0xA7];
+  unk32 _A8;
+  unk8 _AC;
+  unk8 _AD;
+  unk8 _AE;
+  unk32 _B0;
+  unk32 _B4;
 };
 
 } // namespace System
