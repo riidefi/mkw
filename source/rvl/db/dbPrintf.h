a5f7607510319370af0b5e2d175ff81f019d403d
Richard Patel <me@terorie.dev>
2022-07-24 11:39:11 +0200

    Split RVL/DB (#217)

---
+++ source/rvl/db/dbPrintf.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x8015d3ac..0x8015d3fc
+UNKNOWN_FUNCTION(DBPrintf);
+
+#ifdef __cplusplus
+}
+#endif
