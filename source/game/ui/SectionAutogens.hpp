7a5e478aad11a30d2fdcb6feac3e8c1113c97db2
riidefi <34194588+riidefi@users.noreply.github.com>
2023-06-18 14:55:45 -0600

    Split SectionAutogens.cpp

---
+++ source/game/ui/SectionAutogens.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <rk_types.h>
+
+#include <decomp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// PAL: 0x8062c3a4..0x80630094
+UNKNOWN_FUNCTION(Section_addPages);
+// PAL: 0x80630094..0x80631588
+UNKNOWN_FUNCTION(Section_addActivePages);
+// PAL: 0x80631588..0x80631734
+UNKNOWN_FUNCTION(Section_getSceneId);
+// PAL: 0x80631734..0x80631a58
+UNKNOWN_FUNCTION(Section_getResourceName);
+// PAL: 0x80631a58..0x80631af8
+UNKNOWN_FUNCTION(Section_hasBackModel);
+// PAL: 0x80631af8..0x80631bf0
+UNKNOWN_FUNCTION(SectionId_hasDriver);
+// PAL: 0x80631bf0..0x80631c00
+UNKNOWN_FUNCTION(SectionManager_getPageName);
+// PAL: 0x80631c00..0x80631c10
+UNKNOWN_FUNCTION(SectionManager_getSectionName);
+// PAL: 0x80631c10..0x80631c68
+UNKNOWN_FUNCTION(unk_80631c10);
+// PAL: 0x80631c68..0x80631ef8
+UNKNOWN_FUNCTION(unk_80631c68);
+// PAL: 0x80631ef8..0x80632080
+UNKNOWN_FUNCTION(unk_80631ef8);
+// PAL: 0x80632080..0x806321b4
+UNKNOWN_FUNCTION(unk_80632080);
+// PAL: 0x806321b4..0x8063240c
+UNKNOWN_FUNCTION(unk_806321b4);
+
+#ifdef __cplusplus
+}
+#endif
