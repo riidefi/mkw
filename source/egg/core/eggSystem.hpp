d67ac7e045a16c285b8925708b2bbffbacd8a749
Richard Patel <me@terorie.dev>
2023-08-01 03:13:16 +0000

    EGG::SceneManager::destroyScene

--- source/egg/core/eggSystem.hpp
+++ source/egg/core/eggSystem.hpp
@@ -3,6 +3,7 @@
 namespace EGG {
 
 class Video;
+class Heap;
 
 class BaseSystem {
 public:
@@ -10,6 +11,15 @@ public:
 
   //! Returns a pointer to the video manager.
   virtual Video* getVideo() = 0; // [vt+0x08]
+
+public:
+  unsigned int _04;
+  unsigned int _08;
+  unsigned int _0c;
+  unsigned int _10;
+  unsigned int _14;
+  Heap * mRootHeapMem1;
+  Heap * mRootHeapMem2;
 };
 
 } // namespace EGG
