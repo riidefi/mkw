fcfc451c2a2a6e0c2d90ae22594c69b3de3cb9a8
Richard Patel <me@terorie.dev>
2023-08-02 09:44:34 +0000

    EGG::SceneManager::calcCurrentFader
    Co authored by Olifre

--- source/egg/core/eggSceneManager.cpp
+++ source/egg/core/eggSceneManager.cpp
@@ -295,275 +295,107 @@ void SceneManager::calcCurrentScene() {
     this->mCurrentScene->calc();
 }
 
-} // namespace EGG
+static inline void createNewScene(SceneManager* manager, Scene* newScene, int _nextSceneID) {
+  int nextSceneID = _nextSceneID; // manager->getNextSceneID();
+  manager->setupNextSceneID();
+  manager->createScene(nextSceneID, newScene);
+}
 
-#if 0
-namespace EGG {
+static inline void setupNewScene(SceneManager* manager, Scene* currentScene) {
+  Scene* newScene = nullptr;
+  
+  if (currentScene) 
+    newScene = currentScene->getParentScene();
+  
+  if (currentScene) {
+    manager->destroyScene(currentScene);
+    manager->mCurrentScene = nullptr;
+  }
+
+  createNewScene(manager, newScene, manager->mNextSceneID);
+}
 
 void SceneManager::calcCurrentFader() {
   if (!mCurrentFader)
     return;
+
   if (!mCurrentFader->calc())
     return;
+
   switch (this->mTransitionStatus) {
-  case 0: {
-    int r29 = this->mNextSceneID;
-    Scene* curScene;                       // r4
-    while (curScene = this->mCurrentScene) // r4
-    {
-      if (!curScene)
-        continue;
-      Scene* r28 = curScene->getParentScene(); // r28
-      if (r28) {
-        this->destroyScene(r28->getChildScene());
-        this->mNextSceneID = r28->getSceneID();
-        this->setupNextSceneID();
-      } else {
-        this->destroyScene(curScene);
-        this->mNextSceneID = -1;
-        this->setupNextSceneID();
-      }
-    }
-    this->mNextSceneID = r29;
-    EGG::Scene* r30 = 0;
-    if( curScene ) r30 = curScene->getParentScene();
-    if( curScene ) {
-      this->destroyScene(curScene);
-      this->mCurrentScene = 0;
-    }
+    case 0: {
+      int r29 = getNextSceneID();
+      
+      Scene* curScene = nullptr;               // r4
+      while (curScene = this->mCurrentScene) { // r4
+        if (!curScene)
+          continue;
 
-    this->mNextSceneID = r29;
-    this->setupNextSceneID();
-    this->createScene(r29, r30);
-    break;
-  }
-  case 1: {
-    // Identical to changeSiblingScene
-    Scene* r30 = 0;
-    if( mCurrentScene ) r30 = mCurrentScene->getParentScene();
-    if (mCurrentScene) {
-      this->destroyScene(mCurrentScene);
-      this->mCurrentScene = 0;
+        Scene* r28 = curScene->getParentScene(); // r28
+      
+        if (r28) {
+          this->destroyScene(r28->getChildScene());
+          this->mNextSceneID = r28->getSceneID();
+          this->setupNextSceneID();
+        } else {
+          this->destroyScene(curScene);
+          this->mNextSceneID = -1;
+          this->setupNextSceneID();
+        }
+      }
+      
+      this->mNextSceneID = r29;
+
+      setupNewScene(this, curScene);
+      
+      break;
     }
-    int r29 = this->mNextSceneID;
-    this->setupNextSceneID();
-    this->createScene(r29, r30);
-    break;
-  }
-  case 2:
-    this->outgoingParentScene(this->pParent);
-    this->setupNextSceneID();
-    this->createScene(this->get18(), this->pParent);
-    break;
-  case 4:
-    // TODO
-    if (Scene* s = this->findParentScene(this->mNextSceneID)) // r30
-    {
-      while (s->getSceneID() != this->get18()) {
+    case 1: {
+      // Identical to changeSiblingScene
+      setupNewScene(this, mCurrentScene);
+      break;
+    }
+    case 2: {
+      this->outgoingParentScene(this->pParent);
+      this->setupNextSceneID();
+      this->createScene(this->getUnk18(), this->pParent);
+      break;
+    }
+    case 4: {
+      // TODO
+      if (Scene* s = this->findParentScene(this->mNextSceneID)) { // r30
+        while (s->getSceneID() != this->getUnk18()) {
+          if (this->mCurrentScene == 0)
+            continue;
+
+          Scene* r28 = mCurrentScene->getParentScene();
+      
+          if (r28 == nullptr)
+            continue;
+
+          this->destroyScene(r28->getChildScene());
+          this->mNextSceneID = r28->getSceneID();
+          this->setupNextSceneID();
+        }
+
         if (this->mCurrentScene == 0)
-          continue;
-        Scene* r28 = mCurrentScene->getParentScene();
-        if (r28 == 0)
-          continue;
-        this->destroyScene(r28->getChildScene());
-        this->mNextSceneID = r28->getSceneID();
-        this->setupNextSceneID();
+            break;
+  
+        this->mCurrentScene->incoming_childDestroy();
       }
-      if (this->mCurrentScene == 0)
-        break;
-      this->mCurrentScene->incoming_childDestroy();
+  
+      break;
     }
+    case 3: {
+      if (mCurrentScene)
+        mCurrentScene->reinit();
 
-    break;
-  case 3:
-    if (mCurrentScene)
-      mCurrentScene->reinit();
-    break;
+      break;
+    }
   }
-  this->mTransitionStatus = -1;
-}
 
+  this->mTransitionStatus = STATUS_IDLE;
 }
-#else
-
-// Symbol: calcCurrentFader__Q23EGG12SceneManagerFv
-// PAL: 0x8023b5a8..0x8023b800
-MARK_BINARY_BLOB(calcCurrentFader__Q23EGG12SceneManagerFv, 0x8023b5a8,
-                 0x8023b800);
-asm UNKNOWN_FUNCTION(calcCurrentFader__Q23EGG12SceneManagerFv) {
-  // clang-format off
-  nofralloc;
-  stwu r1, -0x20(r1);
-  mflr r0;
-  stw r0, 0x24(r1);
-  stw r31, 0x1c(r1);
-  mr r31, r3;
-  stw r30, 0x18(r1);
-  stw r29, 0x14(r1);
-  stw r28, 0x10(r1);
-  lwz r0, 0x24(r3);
-  cmpwi r0, 0;
-  beq lbl_8023b7e0;
-  mr r3, r0;
-  lwz r12, 0(r3);
-  lwz r12, 0x18(r12);
-  mtctr r12;
-  bctrl;
-  cmpwi r3, 0;
-  beq lbl_8023b7e0;
-  lwz r0, 0x20(r31);
-  cmpwi r0, 0;
-  beq lbl_8023b620;
-  cmpwi r0, 1;
-  beq lbl_8023b6c8;
-  cmpwi r0, 2;
-  beq lbl_8023b714;
-  cmpwi r0, 4;
-  beq lbl_8023b73c;
-  cmpwi r0, 3;
-  beq lbl_8023b7bc;
-  b lbl_8023b7d8;
-lbl_8023b620:
-  lwz r29, 0x14(r31);
-  li r30, -1;
-  b lbl_8023b674;
-lbl_8023b62c:
-  cmpwi r4, 0;
-  beq lbl_8023b674;
-  lwz r28, 0x20(r4);
-  cmpwi r28, 0;
-  beq lbl_8023b660;
-  lwz r4, 0x24(r28);
-  mr r3, r31;
-  bl destroyScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  lwz r0, 0x28(r28);
-  mr r3, r31;
-  stw r0, 0x14(r31);
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-  b lbl_8023b674;
-lbl_8023b660:
-  mr r3, r31;
-  bl destroyScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  stw r30, 0x14(r31);
-  mr r3, r31;
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-lbl_8023b674:
-  lwz r4, 0xc(r31);
-  cmpwi r4, 0;
-  bne lbl_8023b62c;
-  stw r29, 0x14(r31);
-  li r30, 0;
-  beq lbl_8023b690;
-  lwz r30, 0x20(r4);
-lbl_8023b690:
-  cmpwi r4, 0;
-  beq lbl_8023b6a8;
-  mr r3, r31;
-  bl destroyScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  li r0, 0;
-  stw r0, 0xc(r31);
-lbl_8023b6a8:
-  lwz r29, 0x14(r31);
-  mr r3, r31;
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-  mr r3, r31;
-  mr r4, r29;
-  mr r5, r30;
-  bl createScene__Q23EGG12SceneManagerFiPQ23EGG5Scene;
-  b lbl_8023b7d8;
-lbl_8023b6c8:
-  lwz r4, 0xc(r31);
-  li r30, 0;
-  cmpwi r4, 0;
-  beq lbl_8023b6dc;
-  lwz r30, 0x20(r4);
-lbl_8023b6dc:
-  cmpwi r4, 0;
-  beq lbl_8023b6f4;
-  mr r3, r31;
-  bl destroyScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  li r0, 0;
-  stw r0, 0xc(r31);
-lbl_8023b6f4:
-  lwz r29, 0x14(r31);
-  mr r3, r31;
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-  mr r3, r31;
-  mr r4, r29;
-  mr r5, r30;
-  bl createScene__Q23EGG12SceneManagerFiPQ23EGG5Scene;
-  b lbl_8023b7d8;
-lbl_8023b714:
-  lwz r4, 0x10(r31);
-  mr r3, r31;
-  bl outgoingParentScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  mr r3, r31;
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-  lwz r4, 0x18(r31);
-  mr r3, r31;
-  lwz r5, 0x10(r31);
-  bl createScene__Q23EGG12SceneManagerFiPQ23EGG5Scene;
-  b lbl_8023b7d8;
-lbl_8023b73c:
-  lwz r4, 0x14(r31);
-  mr r3, r31;
-  bl findParentScene__Q23EGG12SceneManagerFi;
-  cmpwi r3, 0;
-  mr r30, r3;
-  beq lbl_8023b7d8;
-  b lbl_8023b78c;
-lbl_8023b758:
-  lwz r3, 0xc(r31);
-  cmpwi r3, 0;
-  beq lbl_8023b78c;
-  lwz r28, 0x20(r3);
-  cmpwi r28, 0;
-  beq lbl_8023b78c;
-  lwz r4, 0x24(r28);
-  mr r3, r31;
-  bl destroyScene__Q23EGG12SceneManagerFPQ23EGG5Scene;
-  lwz r0, 0x28(r28);
-  mr r3, r31;
-  stw r0, 0x14(r31);
-  bl setupNextSceneID__Q23EGG12SceneManagerFv;
-lbl_8023b78c:
-  lwz r3, 0x28(r30);
-  lwz r0, 0x18(r31);
-  cmpw r3, r0;
-  bne lbl_8023b758;
-  lwz r3, 0xc(r31);
-  cmpwi r3, 0;
-  beq lbl_8023b7d8;
-  lwz r12, 0(r3);
-  lwz r12, 0x20(r12);
-  mtctr r12;
-  bctrl;
-  b lbl_8023b7d8;
-lbl_8023b7bc:
-  lwz r3, 0xc(r31);
-  cmpwi r3, 0;
-  beq lbl_8023b7d8;
-  lwz r12, 0(r3);
-  lwz r12, 0x1c(r12);
-  mtctr r12;
-  bctrl;
-lbl_8023b7d8:
-  li r0, -1;
-  stw r0, 0x20(r31);
-lbl_8023b7e0:
-  lwz r0, 0x24(r1);
-  lwz r31, 0x1c(r1);
-  lwz r30, 0x18(r1);
-  lwz r29, 0x14(r1);
-  lwz r28, 0x10(r1);
-  mtlr r0;
-  addi r1, r1, 0x20;
-  blr;
-  // clang-format on
-}
-#endif
-
-namespace EGG {
 
 void SceneManager::drawCurrentScene() {
   if (!mCurrentScene)
