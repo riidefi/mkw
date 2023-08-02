97698ff9d7cd1985a30b96aed1da8496c6a8a1cd
Richard Patel <me@terorie.dev>
2023-08-02 11:46:06 +0000

    EGG::Xfb
    Co-authored-by: Aiden <86704247+vabold@users.noreply.github.com>

--- source/egg/core/eggXfb.cpp
+++ source/egg/core/eggXfb.cpp
@@ -1,54 +1,24 @@
 #include "eggXfb.hpp"
+#include <egg/core/eggSystem.hpp>
+#include <egg/core/eggVideo.hpp>
+#include <rvl/gx.h>
 
-// Extern function references.
-// PAL: 0x80229e04
-extern "C" UNKNOWN_FUNCTION(__nwa__FUlPQ23EGG4Heapi);
+namespace EGG {
 
-// Symbol: __ct__Q23EGG3XfbFPQ23EGG4Heap
-// PAL: 0x80244160..0x802441ec
-MARK_BINARY_BLOB(__ct__Q23EGG3XfbFPQ23EGG4Heap, 0x80244160, 0x802441ec);
-asm UNKNOWN_FUNCTION(__ct__Q23EGG3XfbFPQ23EGG4Heap) {
-  // clang-format off
-  nofralloc;
-  stwu r1, -0x10(r1);
-  mflr r0;
-  stw r0, 0x14(r1);
-  stw r31, 0xc(r1);
-  mr r31, r4;
-  stw r30, 8(r1);
-  mr r30, r3;
-  lwz r3, -0x5ca0(r13);
-  lwz r12, 0(r3);
-  lwz r12, 8(r12);
-  mtctr r12;
-  bctrl;
-  lwz r3, 0(r3);
-  lhz r4, 8(r3);
-  lhz r3, 4(r3);
-  sth r3, 0(r30);
-  sth r4, 2(r30);
-  bl calcXfbSize__Q23EGG3XfbFii;
-  cmpwi r31, 0;
-  bne lbl_802441b4;
-  lwz r31, -0x5d60(r13);
-lbl_802441b4:
-  mr r4, r31;
-  li r5, 0x20;
-  bl __nwa__FUlPQ23EGG4Heapi;
-  li r0, 0;
-  stw r3, 4(r30);
-  mr r3, r30;
-  stw r0, 0xc(r30);
-  stw r0, 8(r30);
-  lwz r31, 0xc(r1);
-  lwz r30, 8(r1);
-  lwz r0, 0x14(r1);
-  mtlr r0;
-  addi r1, r1, 0x10;
-  blr;
-  // clang-format on
+Xfb::Xfb(Heap* heap) {
+  Video* video = BaseSystem::sSystem->getVideo();
+  GXRenderModeObj const* mode = video->pRenderMode;
+  setDimensions(mode->fb_width, mode->xfb_height);
+  size_t sz = calcBufferSize(width, height);
+  if (!heap)
+    heap = Heap::getCurrentHeap();
+  buf = new (heap, 0x20) u8[sz];
+  _unk0c = 0u;
+  _unk08 = 0u;
 }
 
-int EGG::Xfb::calcXfbSize(int p1, int p2) {
+size_t EGG::Xfb::calcBufferSize(u16 p1, u16 p2) {
   return (p1 + 0xfu & 0xfff0) * p2 * 2;
 }
+
+} // namespace EGG
