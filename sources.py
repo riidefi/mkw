b9e7a9fcf3154b602de3e7bcd788829cbe552d52
em-eight <m8em.eightm8@gmail.com>
2023-08-20 22:29:11 +0300

    match ModuleSymbols.cpp

--- sources.py
+++ sources.py
@@ -476,6 +476,7 @@ SOURCES_REL = [
     Source(src="source/game/system/RaceManager.cpp", cc='4201_127', opts=REL_OPTS),
     Source(src="source/game/system/ResourceManager.cpp", cc='4201_127', opts=REL_OPTS),
     Source(src="source/game/system/ResourceManagerUnk80542868.cpp", cc='4201_127', opts=REL_OPTS),
+    Source(src="source/game/util/ModuleSymbols.cpp", cc='4201_127', opts=REL_OPTS),
     Source(src="source/game/util/Random.cpp", cc='4201_127', opts=REL_OPTS),
     Source(src="source/game/kart/KartObjectProxy.cpp", cc='4201_127', opts=REL_OPTS),
     Source(src="source/game/geo/ObjKoopaFigure64.cpp", cc='4201_127', opts=REL_OPTS),
