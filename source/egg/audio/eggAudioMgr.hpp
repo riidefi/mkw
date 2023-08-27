89f66ba84d87f54bb2bed53eb037378aaa885f8e
NaraHiero <narahiero@gmail.com>
2023-08-27 12:05:42 -0400

    WIP decomp of RKSystem

--- source/egg/audio/eggAudioMgr.hpp
+++ source/egg/audio/eggAudioMgr.hpp
@@ -26,5 +26,10 @@ public:
 public:
   virtual void initialize();                      // [vt+0x08]
   virtual void calc();                            // [vt+0x0c]
+
+public:
+  u8 _04[0x6b8];
 };
+
+static_assert(sizeof(SimpleAudioMgr) == 0x6bc);
 } // namespace EGG
