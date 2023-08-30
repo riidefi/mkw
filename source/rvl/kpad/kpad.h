4f07330d0389170775e50a086ffcf776995bf043
NaraHiero <narahiero@gmail.com>
2023-08-29 22:02:40 -0400

    Define some KPadController classes + RVL structs

--- source/rvl/kpad/kpad.h
+++ source/rvl/kpad/kpad.h
@@ -4,9 +4,67 @@
 
 #include <decomp.h>
 
+#include <rvl/wpad/wpad.h>
+#include <rvl/mtx/mtx.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+// Size: 0x24
+typedef union KPADEXStatus {
+  struct {
+    Vec2 stick;
+    Vec acc;
+    f32 acc_value;
+    f32 acc_speed;
+  } fs;
+  struct {
+    u32 hold;
+    u32 trig;
+    u32 release;
+    Vec2 lstick;
+    Vec2 rstick;
+    f32 ltrigger;
+    f32 rtrigger;
+  } cl;
+} KPADEXStatus;
+
+// Size: 0x84
+typedef struct KPADStatus {
+  u32 hold;
+  u32 trig;
+  u32 release;
+  Vec acc;
+  f32 acc_value;
+  f32 acc_speed;
+  Vec2 pos;
+  Vec2 vec;
+  f32 speed;
+  Vec2 horizon;
+  Vec2 hori_vec;
+  f32 hori_speed;
+  f32 dist;
+  f32 dist_vec;
+  f32 dist_speed;
+  Vec2 acc_vertical;
+  u8 dev_type;
+  s8 wpad_err;
+  s8 dpd_valid_fg;
+  u8 data_format;
+  KPADEXStatus ex_status;
+} KPADStatus;
+
+// Size: 0x38
+typedef struct KPADUnifiedWpadStatus {
+  union {
+    WPADStatus core;
+    WPADFSStatus fs;
+    WPADCLStatus cl;
+  } u;
+  u8 fmt;
+  u8 padding;
+} KPADUnifiedWpadStatus;
 
 // PAL: 0x801950a0..0x801950b4
 UNKNOWN_FUNCTION(KPADSetFSStickClamp);
