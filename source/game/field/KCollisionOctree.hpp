d74b21d6611983e8e8da30cd429e055d824d934b
em-eight <m8em.eightm8@gmail.com>
2023-11-26 19:07:50 +0200

    Collision test preparation funcs

--- source/game/field/KCollisionOctree.hpp
+++ source/game/field/KCollisionOctree.hpp
@@ -16,8 +16,6 @@ UNKNOWN_FUNCTION(unk_807bdd7c);
 UNKNOWN_FUNCTION(unk_807bddbc);
 // PAL: 0x807bddfc..0x807bdf54
 UNKNOWN_FUNCTION(unk_807bddfc);
-// PAL: 0x807bdf54..0x807be030
-UNKNOWN_FUNCTION(getVertex__Q25Field16KCollisionOctreeFfRCQ23EGG8Vector3fRCQ23EGG8Vector3fRCQ23EGG8Vector3fRCQ23EGG8Vector3f);
 // PAL: 0x807be12c..0x807be3c4
 UNKNOWN_FUNCTION(unk_807be12c);
 // PAL: 0x807be3c4..0x807bf4c0
@@ -32,10 +30,6 @@ UNKNOWN_FUNCTION(kcl_triangle_collides_two_points);
 UNKNOWN_FUNCTION(unk_807c0f00);
 // PAL: 0x807c1514..0x807c1b0c
 UNKNOWN_FUNCTION(kcl_triangle_collides_one_point);
-// PAL: 0x807c1b0c..0x807c1bb4
-UNKNOWN_FUNCTION(unk_807c1b0c);
-// PAL: 0x807c1bb4..0x807c1c8c
-UNKNOWN_FUNCTION(kcl_find_tri_list_and_prepare);
 // PAL: 0x807c1c8c..0x807c1de8
 UNKNOWN_FUNCTION(unk_807c1c8c);
 // PAL: 0x807c1de8..0x807c1f80
@@ -68,7 +62,10 @@ public:
   static EGG::Vector3f getVertex(f32 height, const EGG::Vector3f& vertex1, const EGG::Vector3f& fnrm,
 		  const EGG::Vector3f& enrm3, const EGG::Vector3f& enrm);
   // finds block that contains position
-  s32* searchBlock(const EGG::Vector3f& pos);
+  u16* searchBlock(const EGG::Vector3f& pos);
+  // Sets sphere-octree-collision detection parameters
+  void prepareCollisionTest(const EGG::Vector3f& pos, const EGG::Vector3f& prevPos, u32 typeMask);
+  void prepareCollisionTestSphere(const EGG::Vector3f& pos, const EGG::Vector3f& prevPos, u32 typeMask, f32 radius);
 
 private:
   EGG::Vector3f* pos_data;
@@ -84,5 +81,23 @@ private:
   u32 area_x_blocks_shift;
   u32 area_xy_blocks_shift;
   f32 sphere_radius;
+
+  // --- Sphere-octree collision detection parameters
+  EGG::Vector3f pos;
+  // I think this is for support of directional collision checking (wall is only one way solid)
+  // Pretty much a collision is triggered only when the movement vector has an acute angle with the prism face normal
+  EGG::Vector3f prevPos;
+  EGG::Vector3f movement; // pos - prevPos
+  f32 radius;
+  // This mask can be used to only count collision with certain KCL attribute base types. The n-th bit in this mask signifies if KCL of base type n will count towards collision
+  u32 typeMask;
+  // index of prisms near pos (collision candidates). They exist in the same octree leaf block as pos
+  u16* prismIndexes;
+
+  u8 _[0x290 - 0x6c];
+
+  u16* cachedPrismIndexes;
+  EGG::Vector3f cachedPos;
+  f32 cachedRadius;
 };
 }
