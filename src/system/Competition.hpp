Kevinn1109/mkw2

A bit more Competition documentation

src/system/Competition.hpp
@@ -5,9 +5,56 @@
 
 namespace System {
 
+// More info: https://wiki.tockdom.com/wiki/Mission_Mode#Objectives
+enum MissionObjective {
+  OBJECTIVE_MINITURBO,
+  OBJECTIVE_LAPRUN01,
+  OBJECTIVE_LAPRUN02,
+  OBJECTIVE_DRIFT,
+  OBJECTIVE_ITEMBOX,
+  OBJECTIVE_ENEMYDOWN01,
+  OBJECTIVE_ENEMYDOWN02,
+  OBJECTIVE_ENEMYDOWN03,
+  OBJECTIVE_COINGET01,
+  OBJECTIVE_TOGATE01,
+  OBJECTIVE_ROCKETSTART,
+  OBJECTIVE_ITEMHIT,
+  OBJECTIVE_WHEELIE,
+  OBJECTIVE_SLIPSTREAM
+};
+
 struct CharacterVehicleChoice {
   s8 characterId;
   s8 vehicleId;
+};
+
+struct RawRKCO {
+  char rkcoMagic[4];
+  u16 competitionType;
+  u8 courseId;
+  u8 engineClass;
+  u8 numLaps;
+  u8 field9_0x09;
+  u16 vehicleRestriction;
+  u16 cameraAngle;
+  u16 minimapObject;
+  u16 horizontalWallGlitch;
+  u16 field14_0x12;
+  u16 cannonFlag;
+  u16 cpuCount;
+  CharacterVehicleChoice cpuCombos[11];
+  u8 controllerRestriction;
+  u8 field19_0x2f;
+  u16 objectFiles;
+  u16 score;
+  u8 introSetting;
+  /* Maximum time allowed for the competition record to be uploaded to the rankings.
+     See 80510ab8
+  */
+  u8 field23_0x35;
+  u16 maxUploadTime;
+  u16 field25_0x38;
+  u16 field26_0x3a;
 };
 
 struct RawCompetitionFile {
@@ -15,56 +62,38 @@ struct RawCompetitionFile {
   s32 dataLength;
   s32 headerLength;
   s32 field3_0xc;
-  char rkcoMagic[4];
-  u16 competitionType;
-  u8 courseId;
-  u8 engineClass;
-  u8 numLaps;
-  u8 field9_0x19;
-  u16 vehicleRestriction;
-  u16 cameraAngle;
-  u16 minimapObject;
-  u16 field13_0x20;
-  u16 field14_0x22;
-  u16 cannonFlag;
-  u16 cpuCount;
-  CharacterVehicleChoice cpuCombos[11];
-  u8 controllerRestriction;
-  u8 field19_0x3f;
-  u16 objectFiles;
-  u16 score;
-  u8 s32roSetting;
-  u8 field23_0x45;
-  u16 field24_0x46;
-  u16 field25_0x48;
-  u16 field26_0x4a;
-  inline u16 getSize() { return MIN(11, cpuCount); }
+  RawRKCO rkco;
+  inline u16 getSize() { return MIN(11, rkco.cpuCount); }
 };
 
 struct CompetitionWrapper {
   bool isValid;
   RawCompetitionFile* fileRaw;
-  s32 field5_0x8;
-  s32 field6_0xc;
-  s32 field7_0x10;
-  s32 field8_0x14;
-  s32 field9_0x18;
+  s32 competitionId;
+  s32 state;
+  wchar_t* titleText;
+  s32 titleTextLength;
+  bool competitionUnavailable;
   s32 field10_0x1c;
 };
 
+// Also known as "KMT Scenario Entry". See https://wiki.tockdom.com/wiki/Mission_Mode#mission_single.kmt
 struct CompetitionSettings {
-  u16 field0_0x0;
-  u16 field1_0x2; /* something depending on competition type */
+  u16 mrFile; /* Mission Run file in Mission Mode (e.g. 3 -> mr03.szs) */
+  u16 objective;  /* See the MissionObjective enum */
   u8 courseId;
-  u8 padding[2];
+  u8 characterId;
+  u8 vehicleId;
   u8 engineClass;
-  u8 field5_0x8[39]; /* unknown padding */
+  u8 field5_0x8[36]; /* unknown padding */
+  u16 timeLimit;
+  u8 field_0x2e;
   u8 controllerRestriction;
-  u32 field7_0x30[6]; /* those s32s probably represent amount of coins, gates
-                         etc */
+  u32 scoreRequired;  /* Score required to beat the competition */
+  u32 rankScores[5];  /* Scores required to get a rank (only used in Mission Mode) */
   u16 cameraAngle;
   u16 minimapObject;
-  u16 field10_0x4c;
+  u16 horizontalWallGlitch;
   u16 field11_0x4e;
   u16 cannonFlag;
   u8 field13_0x52[6]; /* unknown padding */

riidefi/mkw

src/system/Competition.hpp
@@ -5,9 +5,56 @@
 
 namespace System {
 
+// More info: https://wiki.tockdom.com/wiki/Mission_Mode#Objectives
+enum MissionObjective {
+  OBJECTIVE_MINITURBO,
+  OBJECTIVE_LAPRUN01,
+  OBJECTIVE_LAPRUN02,
+  OBJECTIVE_DRIFT,
+  OBJECTIVE_ITEMBOX,
+  OBJECTIVE_ENEMYDOWN01,
+  OBJECTIVE_ENEMYDOWN02,
+  OBJECTIVE_ENEMYDOWN03,
+  OBJECTIVE_COINGET01,
+  OBJECTIVE_TOGATE01,
+  OBJECTIVE_ROCKETSTART,
+  OBJECTIVE_ITEMHIT,
+  OBJECTIVE_WHEELIE,
+  OBJECTIVE_SLIPSTREAM
+};
+
 struct CharacterVehicleChoice {
   s8 characterId;
   s8 vehicleId;
+};
+
+struct RawRKCO {
+  char rkcoMagic[4];
+  u16 competitionType;
+  u8 courseId;
+  u8 engineClass;
+  u8 numLaps;
+  u8 field9_0x09;
+  u16 vehicleRestriction;
+  u16 cameraAngle;
+  u16 minimapObject;
+  u16 horizontalWallGlitch;
+  u16 field14_0x12;
+  u16 cannonFlag;
+  u16 cpuCount;
+  CharacterVehicleChoice cpuCombos[11];
+  u8 controllerRestriction;
+  u8 field19_0x2f;
+  u16 objectFiles;
+  u16 score;
+  u8 introSetting;
+  /* Maximum time allowed for the competition record to be uploaded to the rankings.
+     See 80510ab8
+  */
+  u8 field23_0x35;
+  u16 maxUploadTime;
+  u16 field25_0x38;
+  u16 field26_0x3a;
 };
 
 struct RawCompetitionFile {
@@ -15,56 +62,38 @@ struct RawCompetitionFile {
   s32 dataLength;
   s32 headerLength;
   s32 field3_0xc;
-  char rkcoMagic[4];
-  u16 competitionType;
-  u8 courseId;
-  u8 engineClass;
-  u8 numLaps;
-  u8 field9_0x19;
-  u16 vehicleRestriction;
-  u16 cameraAngle;
-  u16 minimapObject;
-  u16 field13_0x20;
-  u16 field14_0x22;
-  u16 cannonFlag;
-  u16 cpuCount;
-  CharacterVehicleChoice cpuCombos[11];
-  u8 controllerRestriction;
-  u8 field19_0x3f;
-  u16 objectFiles;
-  u16 score;
-  u8 s32roSetting;
-  u8 field23_0x45;
-  u16 field24_0x46;
-  u16 field25_0x48;
-  u16 field26_0x4a;
-  inline u16 getSize() { return MIN(11, cpuCount); }
+  RawRKCO rkco;
+  inline u16 getSize() { return min32(11, rkco.cpuCount); }
 };
 
 struct CompetitionWrapper {
   bool isValid;
   RawCompetitionFile* fileRaw;
-  s32 field5_0x8;
-  s32 field6_0xc;
-  s32 field7_0x10;
-  s32 field8_0x14;
-  s32 field9_0x18;
+  s32 competitionId;
+  s32 state;
+  wchar_t* titleText;
+  s32 titleTextLength;
+  bool competitionUnavailable;
   s32 field10_0x1c;
 };
 
+// Also known as "KMT Scenario Entry". See https://wiki.tockdom.com/wiki/Mission_Mode#mission_single.kmt
 struct CompetitionSettings {
-  u16 field0_0x0;
-  u16 field1_0x2; /* something depending on competition type */
+  u16 mrFile; /* Mission Run file in Mission Mode (e.g. 3 -> mr03.szs) */
+  u16 objective;  /* See the MissionObjective enum */
   u8 courseId;
-  u8 padding[2];
+  u8 characterId;
+  u8 vehicleId;
   u8 engineClass;
-  u8 field5_0x8[39]; /* unknown padding */
+  u8 field5_0x8[36]; /* unknown padding */
+  u16 timeLimit;
+  u8 field_0x2e;
   u8 controllerRestriction;
-  u32 field7_0x30[6]; /* those s32s probably represent amount of coins, gates
-                         etc */
+  u32 scoreRequired;  /* Score required to beat the competition */
+  u32 rankScores[5];  /* Scores required to get a rank (only used in Mission Mode) */
   u16 cameraAngle;
   u16 minimapObject;
-  u16 field10_0x4c;
+  u16 horizontalWallGlitch;
   u16 field11_0x4e;
   u16 cannonFlag;
   u8 field13_0x52[6]; /* unknown padding */